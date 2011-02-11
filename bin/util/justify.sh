b6b5c6a08a5c114fd5cd078b59ef5cc2f2b4cb59
Timothy Lebo <timrdf@gmail.com>
2011-02-11 15:36:06 -0500

    new offers for justify.sh inference engines; jar handles codebooked URIs.

--- bin/util/justify.sh
+++ bin/util/justify.sh
@@ -11,6 +11,7 @@ if [ $# -ne 3 ]; then
    echo $usage_message 
    echo "   engine-name: xls2csv, tab2comma, redelimit, file_rename, escaping_commas_redelimit etc. (URI-friendly)"
    echo "                duplicate, google_refine, serialization_change, parse_field, tabulating_fixed_width"
+   echo "                html_tidy pretty_print"
    exit 1
 fi
 
