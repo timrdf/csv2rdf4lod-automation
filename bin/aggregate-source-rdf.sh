d99b3dc6d664a8f048369ec1353b262be8c9119d
Timothy Lebo <timrdf@gmail.com>
2011-03-02 10:36:56 -0500

    added output to aggregate-source-rdf.sh

--- bin/aggregate-source-rdf.sh
+++ bin/aggregate-source-rdf.sh
@@ -11,17 +11,23 @@ fi
 while [ $# -gt 1 ]; do
    rdfFile="$1"
 
+   echo "source/$rdfFile.graph"
    if [ ! -e source/$rdfFile.graph ]; then
       cr-dataset-uri.sh | grep "^h" | tail -1 > source/$rdfFile.graph
    else 
       echo "WARNING: publish/$rdfFile.void.ttl existed; not replacing"
    fi
 
+   echo "publish/$rdfFile.void.ttl"
    if [ ! -e publish/$rdfFile.void.ttl ]; then
       cr-dataset-uri.sh void > publish/$rdfFile.void.ttl
    else 
       echo "WARNING: publish/$rdfFile.void.ttl existed; not replacing"
    fi
+
+   echo "publish/bin/virtuoso-load.sh"
    echo "sudo /opt/virtuoso/scripts/vload ttl source/crawl.ttl `cat source/$rdfFile.graph`" > publish/bin/virtuoso-load.sh
+
+   echo "publish/bin/virtuoso-load-metadata.sh"
    echo "sudo /opt/virtuoso/scripts/vload ttl publish/crawl.ttl.void.ttl http://logd.tw.rpi.edu/vocab/Dataset" > publish/bin/virtuoso-load-metadata.sh
 done
