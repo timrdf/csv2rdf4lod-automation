d1502c66559b2d1d0ede4693a0f9155d03371155
Timothy Lebo <timrdf@gmail.com>
2011-03-02 10:33:21 -0500

    first step toward publishing rdf-from-start instead of rdf-via-csv

---
+++ bin/aggregate-source-rdf.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+   echo "usage: `basename $0` source/some.{rdf,ttl,nt}"
+fi
+
+if [ ! -d publish/bin ]; then
+   mkdir -p publish/bin
+fi
+
+while [ $# -gt 1 ]; do
+   rdfFile="$1"
+
+   if [ ! -e source/$rdfFile.graph ]; then
+      cr-dataset-uri.sh | grep "^h" | tail -1 > source/$rdfFile.graph
+   else 
+      echo "WARNING: publish/$rdfFile.void.ttl existed; not replacing"
+   fi
+
+   if [ ! -e publish/$rdfFile.void.ttl ]; then
+      cr-dataset-uri.sh void > publish/$rdfFile.void.ttl
+   else 
+      echo "WARNING: publish/$rdfFile.void.ttl existed; not replacing"
+   fi
+   echo "sudo /opt/virtuoso/scripts/vload ttl source/crawl.ttl `cat source/$rdfFile.graph`" > publish/bin/virtuoso-load.sh
+   echo "sudo /opt/virtuoso/scripts/vload ttl publish/crawl.ttl.void.ttl http://logd.tw.rpi.edu/vocab/Dataset" > publish/bin/virtuoso-load-metadata.sh
+done
