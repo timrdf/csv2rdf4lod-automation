45e7a33144b02c03ba8ed4cf664315794bde4da4
Timothy Lebo <timrdf@gmail.com>
2011-03-04 23:18:34 -0500

    added time to md5 URIs name and added xsd:date to it

--- bin/util/nfo-filehash.sh
+++ bin/util/nfo-filehash.sh
@@ -3,14 +3,15 @@
 while [ $# -gt 0 ]; do
    file="$1"
    fileMD5=`$CSV2RDF4LOD_HOME/bin/util/md5.sh $file`
+   date_id=`date +%s`
 
    echo "<$file>"                                                                    
    echo "   a pmlp:Information;"                                                    
-   echo "   nfo:hasHash <md5_$fileMD5>;"                                
+   echo "   nfo:hasHash <md5_${fileMD5}_time_${date_id}>;"                                
    echo "."                                                                      
-   echo ""                                                                      
-   echo "<md5_$fileMD5>"                                             
+   echo "<md5_${fileMD5}_time_${date_id}>"                                             
    echo "   a nfo:FileHash; "                                                 
+   echo "   dcterms:date \"`dateInXSDDateTime.sh`\"^^xsd:dateTime;"
    echo "   nfo:hashAlgorithm \"md5\";"                                      
    echo "   nfo:hashValue \"$fileMD5\";"                          
    echo "."                                                                
