e59c63753dfe0e1e69e612b582db1a44ff23de45
Timothy Lebo <timrdf@gmail.com>
2011-03-20 10:39:57 -0400

    pvload.sh file name hiccup

--- bin/util/pvload.sh
+++ bin/util/pvload.sh
@@ -140,16 +140,19 @@ while [ $# -gt 0 ]; do
    echo --------------------------------------------------------------------------------
 
    vload=${CSV2RDF4LOD_PUBLISH_VIRTUOSO_SCRIPT_PATH:-"/opt/virtuoso/scripts/vload"}
+   echo $assudo $vload nt ${TEMP}.pml.ttl                 $named_graph
    if [ ${dryrun-"."} != "true" ]; then
       $assudo $vload nt ${TEMP}.pml.ttl                 $named_graph # Provenance of response (SourceUsage created by pcurl.sh).
-      $assudo $vload nt ${TEMP}${unzipped}.nt           $named_graph #        Actual response (in ntriples syntax).
-      $assudo $vload nt ${TEMP}${unzipped}.load.pml.ttl $named_graph # Provenance of loading file into the store. TODO: cat ${TEMP}${unzipped}.load.pml.ttl into a pmlp:hasRawString?
-      # TODO: rm ${TEMP} ${TEMP}.pml.ttl ${TEMP}${unzipped}.nt ${TEMP}${unzipped}.load.pml.ttl
-   else
-      echo "$assudo $vload nt $TEMP.pml.ttl                   $named_graph"
-      echo "$assudo $vload nt ${TEMP}${unzipped}.nt           $named_graph"
-      echo "$assudo $vload nt ${TEMP}${unzipped}.load.pml.ttl $named_graph"
    fi
+   echo $assudo $vload nt ${TEMP}${unzipped}.nt           $named_graph
+   if [ ${dryrun-"."} != "true" ]; then
+      $assudo $vload nt ${TEMP}${unzipped}.nt           $named_graph #        Actual response (in ntriples syntax).
+   fi
+   echo $assudo $vload nt ${TEMP}.load.pml.ttl $named_graph
+   if [ ${dryrun-"."} != "true" ]; then
+      $assudo $vload nt ${TEMP}.load.pml.ttl $named_graph            # Provenance of loading file into the store. TODO: cat ${TEMP}${unzipped}.load.pml.ttl into a pmlp:hasRawString?
+   fi
+   # TODO: rm ${TEMP} ${TEMP}.pml.ttl ${TEMP}${unzipped}.nt ${TEMP}${unzipped}.load.pml.ttl
 
    shift
 done
