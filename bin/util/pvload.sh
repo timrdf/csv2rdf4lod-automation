8bb3ae36950fe1fb936e222971cef1fcae190e68
Timothy Lebo <timrdf@gmail.com>
2011-03-20 10:46:03 -0400

    why are tools not smart enough to detect which syntax!

--- bin/util/pvload.sh
+++ bin/util/pvload.sh
@@ -142,7 +142,7 @@ while [ $# -gt 0 ]; do
    vload=${CSV2RDF4LOD_PUBLISH_VIRTUOSO_SCRIPT_PATH:-"/opt/virtuoso/scripts/vload"}
    echo $assudo $vload nt ${TEMP}.pml.ttl       $named_graph
    if [ ${dryrun-"."} != "true" ]; then # Provenance of response (SourceUsage created by pcurl.sh).
-      $assudo $vload nt ${TEMP}.pml.ttl         $named_graph 2>1 | grep -v "Loading triples into graph"
+      $assudo $vload ttl ${TEMP}.pml.ttl        $named_graph 2>1 | grep -v "Loading triples into graph"
       cat /tmp/virtuoso-tmp/vload.log
    fi
    echo $assudo $vload nt ${TEMP}${unzipped}.nt $named_graph
@@ -152,7 +152,7 @@ while [ $# -gt 0 ]; do
    fi
    echo $assudo $vload nt ${TEMP}.load.pml.ttl  $named_graph
    if [ ${dryrun-"."} != "true" ]; then # Provenance of loading file into the store. TODO: cat ${TEMP}${unzipped}.load.pml.ttl into a pmlp:hasRawString?
-      $assudo $vload nt ${TEMP}.load.pml.ttl    $named_graph 2>1 | grep -v "Loading triples into graph"             
+      $assudo $vload ttl ${TEMP}.load.pml.ttl   $named_graph 2>1 | grep -v "Loading triples into graph"             
       cat /tmp/virtuoso-tmp/vload.log
    fi
    # TODO: rm ${TEMP} ${TEMP}.pml.ttl ${TEMP}${unzipped}.nt ${TEMP}${unzipped}.load.pml.ttl
