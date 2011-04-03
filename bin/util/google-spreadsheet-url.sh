f72f496a142747c9b380341dfa00b16aacbf556f
Timothy Lebo <timrdf@gmail.com>
2011-04-03 11:35:06 -0400

    new url construction in bin/util/google-spreadsheet-url.sh

--- bin/util/google-spreadsheet-url.sh
+++ bin/util/google-spreadsheet-url.sh
@@ -5,4 +5,5 @@ if [ $# -lt 1 ]; then
    echo "usage: `basename $0` spreadsheet-key"
 fi
 
-echo 'http://spreadsheets.google.com/tq?tqx=out:csv&tq=select%20*&key='$1
+#echo 'http://spreadsheets.google.com/tq?tqx=out:csv&tq=select%20*&key='$1
+echo "http://spreadsheets.google.com/pub?key=$1&output=csv"
