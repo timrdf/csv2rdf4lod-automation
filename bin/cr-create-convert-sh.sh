618cbdf3b315af6982cc3c81f5421538f1c26308
Timothy Lebo <timrdf@gmail.com>
2011-04-11 18:05:33 -0400

    added parameters to cr-create-convert-sh.sh, cleaned subjectDiscrim logic, removed old $formats/csv/bin call

--- bin/cr-create-convert-sh.sh
+++ bin/cr-create-convert-sh.sh
@@ -16,17 +16,38 @@
 #                    ^^ dataset                        ^
 #                                                      ^^ version  
 if [ $# -lt 1 ]; then
-   echo "usage: `basename $0` [-w] [--delimiter delimiter] [-s sourceIdentifier] [-d datasetIdentifier] [-v versionIdentifier] a.csv [another.csv ...]\\n
+   echo "usage: `basename $0` [-w] [--comment-character char] [--header-line row] [--delimiter delimiter] [-s sourceIdentifier] [-d datasetIdentifier] [-v versionIdentifier] a.csv [another.csv ...]\\n
 run from csv2rdf4lod/data/source/SSS/version/VVV/"
    exit 1
 fi
-
 
 if [ "$1" == "-w" ]; then
    writeSH=yes
    shift
 else 
    writeSH=no
+fi
+
+commentCharacter=""
+if [ ${1:-"."} == "--comment-character" ]; then
+   if [ $# -gt 2 -a $2 != "--delimiter" ]; then
+      commentCharacter="$2"
+      shift 2
+   else
+      shift
+      echo "WARNING: did not recognize --comment-character"
+   fi
+fi
+
+headerRow=""
+if [ ${1:-"."} == "--header-line" ]; then
+   if [ $# -gt 2 -a $2 != "--delimiter" ]; then
+      headerRow="$2"
+      shift 2
+   else
+      shift
+      echo "WARNING: did not recognize --header-line"
+   fi
 fi
 
 cellDelimiter=","
@@ -69,7 +90,7 @@ CSV2RDF4LOD_BASE_URI=${CSV2RDF4LOD_BASE_URI:?"not set; source csv2rdf4lod/source
 
 TMP_SH=`date +%s`_$$.sh.tmp
 
-echo "#!/bin/bash"                                                        > $TMP_SH
+echo "#!/bin/bash"                                                      > $TMP_SH
 echo "# $datasetID $versionID ($lastModDate)"                          >> $TMP_SH
 
 echo "#--------------------------------------------------------------" >> $TMP_SH
@@ -80,7 +101,7 @@ echo "surrogate=\"$CSV2RDF4LOD_BASE_URI\" # Came from \$CSV2RDF4LOD_BASE_URI whe
 echo "sourceID=\"$sourceID\""                                          >> $TMP_SH
 echo "datasetID=\"$datasetID\""                                        >> $TMP_SH
 echo "datasetVersion=\"$versionID\"        # NO SPACES; Use curl -I -L http://www.data.gov/download/$datasetID/csv | grep "Last-Modified:" | awk '{printf("%s-%s-%s",$3,$4,$5)}'" >> $TMP_SH
-echo "versionID=\"$versionID\"        # renaming datasetVersion (deprecating datasetVersion)"       >> $TMP_SH
+echo "versionID=\"$versionID\"             # renaming datasetVersion (deprecating datasetVersion)"       >> $TMP_SH
 echo "eID=\"1\"                             # enhancement identifier"  >> $TMP_SH
 echo "if [ \$# -ge 2 ]; then"                                          >> $TMP_SH
 echo "   if [ \$1 == \"-e\" ]; then"                                   >> $TMP_SH
@@ -108,22 +129,22 @@ while [ $# -gt 0 ]; do
    echo "#-----------------------------------"                         >> $TMP_SH
    echo "datafile=\"`basename "$artifact"`\""                          >> $TMP_SH
    echo "data=\"\$sourceDir/\$datafile\""                              >> $TMP_SH
+   subjectDiscriminator=""
    if [ $includeDiscriminator == "yes" ]; then
       extensionlessFilename=`basename $artifact | sed 's/^\([^\.]*\)\..*$/\1/'` # failed to cast 'ce.supersector.csv' to 'ce.supersector'
       extensionlessFilename=`basename $artifact | sed 's/\.[^.]*$//'`
       # clean up %20 by replacing with underscore.
-      echo "subjectDiscriminator=\"`echo $extensionlessFilename | sed 's/%20/-/g; s/_/-/g' | awk '{print tolower($0)}'`\" # Additional part of URI for subjects created; must be URI-ready (e.g., no spaces)." >> $TMP_SH
-   else
-      echo "subjectDiscriminator=               # Additional part of URI for subjects created; must be URI-ready (e.g., no spaces)." >> $TMP_SH
+      subjectDiscriminator=`echo $extensionlessFilename | sed 's/%20/-/g; s/_/-/g' | awk '{print tolower($0)}'`
    fi
+   echo "subjectDiscriminator=\"$subjectDiscriminator\"             # Additional part of URI for subjects created; must be URI-ready (e.g., no spaces)." >> $TMP_SH
+   echo "commentCharacter=\"$commentCharacter\"                 # ONLY one character; complain to http://sourceforge.net/projects/javacsv/ otherwise." >> $TMP_SH
    echo "cellDelimiter=\"$cellDelimiter\"                   # ONLY one character; complain to http://sourceforge.net/projects/javacsv/ otherwise." >> $TMP_SH
-   echo "header=                             # Line that header is on; only needed if not '1'. '0' means no header." >> $TMP_SH
+   echo "header=$headerRow                             # Line that header is on; only needed if not '1'. '0' means no header." >> $TMP_SH
    echo "dataStart=                          # Line that data starts; only needed if not immediately after header." >> $TMP_SH
    echo "repeatAboveIfEmptyCol=              # 'Fill in' value from row above for this column."                     >> $TMP_SH
    echo "onlyIfCol=                          # Do not process if value in this column is empty"                     >> $TMP_SH
    echo "interpretAsNull=                    # NO SPACES"              >> $TMP_SH
    echo "dataEnd=                            # Line on which data stops; only needed if non-data bottom matter (legends, footnotes, etc)." >> $TMP_SH
-   #echo "source \$formats/csv/bin/convert.sh"                         >> $TMP_SH
    echo "source \$CSV2RDF4LOD_HOME/bin/convert.sh"                     >> $TMP_SH
 
    shift
@@ -132,7 +153,6 @@ done
 echo ""                                                                >> $TMP_SH
 echo ""                                                                >> $TMP_SH
 echo "#-----------------------------------"                            >> $TMP_SH
-#echo "source \$formats/csv/bin/convert-aggregate.sh"                  >> $TMP_SH
 echo "source \$CSV2RDF4LOD_HOME/bin/convert-aggregate.sh"              >> $TMP_SH
 
 if [ $writeSH == "yes" ]; then
