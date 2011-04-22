706efcf6e6fbdb95c40863fb94b70f17785c0dba
Timothy Lebo <timrdf@gmail.com>
2011-04-22 09:16:51 -0400

    added comment pointing to google spreadsheet

--- doc/examples/source/dpdoughtroy-com/menu-on-wall-transcription/version/2source.sh
+++ doc/examples/source/dpdoughtroy-com/menu-on-wall-transcription/version/2source.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+#
+# This script will set up a new version of the dpdoughtroy-com / menu-on-wall-transcription dataset 
+# by retrieving the Google Spreadsheet at
+# https://spreadsheets.google.com/spreadsheet/ccc?pli=1&hl=en&key=t7_5xgxUiHibGpbWFPtVQLA&hl=en#gid=0
+
 google2source.sh -w t7_5xgxUiHibGpbWFPtVQLA auto
