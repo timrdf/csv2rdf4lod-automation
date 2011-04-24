328f8aa5d859b6e9b1d4b0982e42de54bab4d02d
Timothy Lebo <timrdf@gmail.com>
2011-04-24 15:17:47 -0400

    separated which image each zone was transcribed from. added foaf:depiction and dc:source

--- doc/examples/source/dpdoughtroy-com/menu-on-wall-transcription/version/2source.sh
+++ doc/examples/source/dpdoughtroy-com/menu-on-wall-transcription/version/2source.sh
@@ -3,6 +3,11 @@
 # This script will set up a new version of the dpdoughtroy-com / menu-on-wall-transcription dataset 
 # by retrieving the Google Spreadsheet at:
 # https://spreadsheets.google.com/spreadsheet/ccc?pli=1&hl=en&key=t7_5xgxUiHibGpbWFPtVQLA&hl=en#gid=0
+#
+# and applying the enhancement parameters at:
+# https://github.com/timrdf/csv2rdf4lod-automation/blob/master/doc/examples/source/dpdoughtroy-com/menu-on-wall-transcription/version/menu-on-wall-transcription.csv.e1.params.ttl
+#
+# to produce RDF.
 
 CSV2RDF4LOD_CONVERT_OMIT_RAW_LAYER=true
 google2source.sh -w t7_5xgxUiHibGpbWFPtVQLA auto
