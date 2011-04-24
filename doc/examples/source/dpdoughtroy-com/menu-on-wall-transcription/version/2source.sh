7f9c976194c91dbbb7a4a3baadc17a70b158496f
Timothy Lebo <timrdf@gmail.com>
2011-04-24 14:43:53 -0400

    menu-on-wall-transcription.csv.e1.params.ttl - abbreviated; added equiv-prop to rdfs:seeAlso and and sub-prop to dct:source 2source.sh - added CSV2RDF4LOD_CONVERT_OMIT_RAW_LAYER=true

--- doc/examples/source/dpdoughtroy-com/menu-on-wall-transcription/version/2source.sh
+++ doc/examples/source/dpdoughtroy-com/menu-on-wall-transcription/version/2source.sh
@@ -4,4 +4,5 @@
 # by retrieving the Google Spreadsheet at:
 # https://spreadsheets.google.com/spreadsheet/ccc?pli=1&hl=en&key=t7_5xgxUiHibGpbWFPtVQLA&hl=en#gid=0
 
+CSV2RDF4LOD_CONVERT_OMIT_RAW_LAYER=true
 google2source.sh -w t7_5xgxUiHibGpbWFPtVQLA auto
