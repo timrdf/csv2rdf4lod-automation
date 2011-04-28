051f7ba3337ca336259097695e1fc50f86a6e447
Timothy Lebo <timrdf@gmail.com>
2011-04-28 16:39:50 -0400

    added 2source.sh link to itself.

--- doc/examples/source/dpdoughtroy-com/menu-on-box-transcription/version/2source.sh
+++ doc/examples/source/dpdoughtroy-com/menu-on-box-transcription/version/2source.sh
@@ -5,7 +5,7 @@
 # https://spreadsheets.google.com/spreadsheet/ccc?pli=1&hl=en&key=t5up1fIZHQWCl8A2frpfC_w&hl=en#gid=0
 #
 # and applying the enhancement parameters at:
-# TODO
+# https://github.com/timrdf/csv2rdf4lod-automation/blob/master/doc/examples/source/dpdoughtroy-com/menu-on-box-transcription/version/2source.sh
 #
 # to produce RDF.
 
