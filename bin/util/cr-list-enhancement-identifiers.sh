b98a3a3e2f605adef25fa9d21d10661d4b6cd989
Timothy Lebo <timrdf@gmail.com>
2011-05-19 19:37:00 -0400

    got rid of extra fail condition of being in the right directory

--- bin/util/cr-list-enhancement-identifiers.sh
+++ bin/util/cr-list-enhancement-identifiers.sh
@@ -2,13 +2,17 @@
 
 back_one=`cd .. 2>/dev/null && pwd`
 ANCHOR_SHOULD_BE_A_VERSION=`basename $back_one` # Use the names from the canonical directory structure
-if [ ! -d automatic -o $ANCHOR_SHOULD_BE_A_VERSION != "version" ]; then
+#if [ ! -d automatic -o $ANCHOR_SHOULD_BE_A_VERSION != "version" ]; then
+if [ ! -d automatic ]; then
    exit 1
 fi
 
 # WARNING: only handles e1 through e9
 
-find automatic -name "*.e[!.].ttl" | sed -e 's/^.*\.e\([^.]*\).ttl/\1/' | sort -u
+# e.g.
+# automatic/streams_all.csv.e1.ttl -> 1
+#
+find automatic -name  "*.e[!.].ttl" | sed -e 's/^.*\.e\([^.]*\).ttl/\1/' | sort -u
 
 # This would be great, except it might get a parameter that hasn't been modified yet:
 #find manual -name "*.params.ttl" | sed -e 's/^.*\.\(.*\)\.params.ttl$/\1/' | sort -ru
