297cfe0ff6cf99da012498f39bc8f1bf1d1ee116
Timothy Lebo <timrdf@gmail.com>
2011-05-20 13:19:32 -0400

    added coin:slug param to dateInXSDDateTime.sh

--- bin/util/dateInXSDDateTime.sh
+++ bin/util/dateInXSDDateTime.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+#
+# Give any params to get URI-friendly coin:slug
+# see https://github.com/timrdf/csv2rdf4lod-automation/wiki/CoIN:-Composition-of-Identifier-Names
 
-date +%Y-%m-%dT%H:%M:%S%z | sed 's/^\(.*\)\(..\)$/\1:\2/'
+if [ $# -eq 0 ]; then
+   date +%Y-%m-%dT%H:%M:%S%z | sed 's/^\(.*\)\(..\)$/\1:\2/'
+elif [ $1 == "coin:slug" ]; then
+   date +%Y-%m-%dT%H_%M_%S%z | sed 's/^\(.*\)\(..\)$/\1:\2/' | sed 's/:/-/g'
+else
+   date +%Y-%m-%dT%H:%M:%S%z | sed 's/^\(.*\)\(..\)$/\1:\2/'
+fi
