d2e1cd78f8e5a34d96c407d05d5d0da4c78073ac
Timothy Lebo <timrdf@gmail.com>
2011-05-27 04:12:55 -0400

    added override for URL fetch in virtuoso load script creation.

--- bin/convert-aggregate.sh
+++ bin/convert-aggregate.sh
@@ -689,7 +689,7 @@ echo ""                                                                         
 echo "allNT=$allNT # to cite graph"                                                             >> $vloadSH
 echo "graph=\"\`cat \$allNT.graph\`\""                                                          >> $vloadSH
 echo "if [ \"\$1\" == \"--sample\" ]; then"                                                     >> $vloadSH
-http_allRawSample="${CSV2RDF4LOD_BASE_URI}/source/${sourceID}/file/${datasetID}/version/${versionID}/conversion/${sourceID}-${datasetID}-${datasetVersion}.rdf"
+http_allRawSample="\${CSV2RDF4LOD_BASE_URI_OVERRIDE:-\$CSV2RDF4LOD_BASE_URI}/source/${sourceID}/file/${datasetID}/version/${versionID}/conversion/${sourceID}-${datasetID}-${datasetVersion}.rdf"
 for conversionStep in $conversionSteps # <---- Add root-level subsets here.
 do
    layerID=`echo $conversionStep | sed 's/^.*\//e/'`
@@ -698,7 +698,7 @@ do
    echo "   sampleGraph=\"\$graph/conversion/\$conversionStep/subset/sample\""                  >> $vloadSH
    echo "   #sudo /opt/virtuoso/scripts/vload ttl \$sampleTTL \$sampleGraph"                     >> $vloadSH
    #http://logd.tw.rpi.edu/source/twc-rpi-edu/file/instance-hub-us-states-and-territories/version/2011-Mar-31_17-51-07/conversion/twc-rpi-edu-instance-hub-us-states-and-territories-2011-Mar-31_17-51-07.e1.sample
-   echo "   sampleURL=\"${CSV2RDF4LOD_BASE_URI}/source/${sourceID}/file/${datasetID}/version/${versionID}/conversion/${sourceID}-${datasetID}-${datasetVersion}.${layerID}.sample\"" >> $vloadSH
+   echo "   sampleURL=\"\${CSV2RDF4LOD_BASE_URI_OVERRIDE:-\$CSV2RDF4LOD_BASE_URI}/source/${sourceID}/file/${datasetID}/version/${versionID}/conversion/${sourceID}-${datasetID}-${datasetVersion}.${layerID}.sample\"" >> $vloadSH
    echo "   echo \${CSV2RDF4LOD_HOME}/bin/util/pvload.sh \$sampleURL -ng \$sampleGraph"         >> $vloadSH
    echo "   \${CSV2RDF4LOD_HOME}/bin/util/pvload.sh \$sampleURL -ng \$sampleGraph"             >> $vloadSH
    echo ""                                                                                      >> $vloadSH
