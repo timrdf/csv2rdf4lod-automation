db7b16060fda547c83620a55959ccd0888952ddd
Timothy Lebo <timrdf@gmail.com>
2011-05-29 11:24:32 -0400

    squashing + in dateTime coin:slug

--- bin/util/dateInXSDDateTime.sh
+++ bin/util/dateInXSDDateTime.sh
@@ -6,7 +6,7 @@
 if [ $# -eq 0 ]; then
    date +%Y-%m-%dT%H:%M:%S%z | sed 's/^\(.*\)\(..\)$/\1:\2/'
 elif [ $1 == "coin:slug" ]; then
-   date +%Y-%m-%dT%H_%M_%S%z | sed 's/^\(.*\)\(..\)$/\1:\2/' | sed 's/:/-/g'
+   date +%Y-%m-%dT%H_%M_%S%z | sed 's/^\(.*\)\(..\)$/\1:\2/' | sed 's/:/-/g; s/\+/-/g'
 else
    date +%Y-%m-%dT%H:%M:%S%z | sed 's/^\(.*\)\(..\)$/\1:\2/'
 fi
