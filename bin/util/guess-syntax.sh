7f90de02e0db8a9ea08b55b2fdbe70385039feb1
Timothy Lebo <timrdf@gmail.com>
2011-05-30 20:34:55 -0400

    added --inspect option to guess-syntax.sh

--- bin/util/guess-syntax.sh
+++ bin/util/guess-syntax.sh
@@ -4,24 +4,41 @@
 # exit 1 if it does not.
 
 if [ $# -lt 1 ]; then
-   echo "usage: `basename $0` url-of.rdf [tool:{rapper,jena}]"
+   echo "usage: `basename $0` [--inspect] url-of.rdf [tool:{rapper,jena}]"
+   echo "  --inspect: look at the local file and guess (if not specified, guesses based on the file name)."
    exit 1
+fi
+
+inspect="false"
+if [ $1 == "--inspect" ]; then
+   inspect="true"
+   shift
 fi
 
 url=$1
 tool=$2
 
+guess=""
+
 if [ ${tool:-"."} == "rapper" ]; then
    if [[ $url =~ \.nt$ || $url =~ \.nt\. || $url =~ \.nt# ]]; then
-      echo "-i ntriples"
+      guess="-i ntriples"
    elif [[ $url =~ \.ttl$ || $url =~ \.ttl\. || $url =~ \.ttl# ]]; then
-      echo "-i turtle"
+      guess="-i turtle"
    elif [[ $url =~ \.rdf$ || $url =~ \.rdf\. || $url =~ \.rdf# ]]; then
-      echo "-i rdfxml"
+      guess="-i rdfxml"
    else
       # We failed to guess based on the file name.
-      # rdf:about=
-      echo "-g"
+      guess="-g"
+   fi
+   if [ $inspect == "true" ]; then
+      if [[ `head -1000 $url | awk '$0 ~ "rdf:about" {c++} END {printf("%s",c)}'` > 5 ]]; then
+         # rdf:about=
+         guess="-i rdfxml"
+      fi
+   fi
+   echo $guess
+   if [[ $guess == "-g" ]]; then
       exit 1
    fi
 elif [ ${tool:-"."} == "rapper" ]; then
