cff2add6715a0e9ed434056ee1826cc6398a5f87
Timothy Lebo <timrdf@gmail.com>
2011-05-30 23:01:06 -0400

    mirror-endpoint.sh

---
+++ bin/util/mirror-endpoint.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#
+# See https://github.com/timrdf/csv2rdf4lod-automation/wiki/Named-graphs-that-know-where-they-came-from
+
+if [[ $# -lt 2 ]]; then
+   echo "usage: `basename $0` <endpoint> <named_graph> [named_graph] ..."
+   exit 1
+fi
+
+CSV2RDF4LOD_HOME=${CSV2RDF4LOD_HOME:?"not set; source csv2rdf4lod/source-me.sh or see https://github.com/timrdf/csv2rdf4lod-automation/wiki/CSV2RDF4LOD-not-set"}
+
+endpoint=$1
+shift
+
+while [ $# -gt 0 ]; do
+   named_graph=$1
+   echo grabbing $named_graph from $endpoint
+
+   TEMP_query="_"`basename $0``date +%s`_$$.rq
+   TEMP_results="_"`basename $0``date +%s`_$$.tmp
+  
+   #
+   # Query it in via cache-queries (this models the endpoint and query explicitly).
+   #
+   echo "construct { ?s ?p ?o } where { graph <$named_graph> {?s ?p ?o} }" > $TEMP_query
+
+   #                                                       \/        \/ hacks applies only to Virtuoso
+   ${CSV2RDF4LOD_HOME}/bin/util/cache-queries.sh $endpoint -p format -o xml -q $TEMP_query -od $TEMP_results
+  
+   ${CSV2RDF4LOD_HOME}/bin/util/virtuoso/vload rdf $TEMP_results/$TEMP_QUERY.xml         $named_graph # NOTE: No provenance of this load; done with pvload.sh below.
+   ${CSV2RDF4LOD_HOME}/bin/util/virtuoso/vload rdf $TEMP_results/$TEMP_QUERY.xml.pml.ttl $named_graph # NOTE: No provenance of this load
+
+   queryURL=`grep "^<.*query=construct" $TEMP_results/$TEMP_QUERY.xml.pml.ttl | awk '{gsub(/<|>/,"");print}' | head -1` # grepping when should be sparqling...
+  
+   #
+   # Query it in directly (will connect up with previous provenance via queryURL)
+   #
+   if [ ${#queryURL} -gt 0 ]; then
+      ${CSV2RDF4LOD_HOME}/bin/util/pvload.sh $queryURL $named_graph # Queries endpoint again.
+   fi
+
+   rm -rf $TEMP_query $TEMP_results &> /dev/null
+
+   shift
+done
