dde58cd63906c416b7ee26cfab7275a0bc87fbcb
Timothy Lebo <timrdf@gmail.com>
2011-05-31 16:35:58 -0400

    ODP publications example.

---
+++ doc/examples/source/iodp-org/publications/version/2source.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# Script to retrieve and convert a new version of the dataset.
+# See https://github.com/timrdf/csv2rdf4lod-automation/wiki/Automated-creation-of-a-new-Versioned-Dataset
+
+export CSV2RDF4LOD_CONVERT_OMIT_RAW_LAYER="true"
+$CSV2RDF4LOD_HOME/bin/cr-create-versioned-dataset-dir.sh cr:auto                                                 \
+                                                        'http://dl.dropbox.com/u/20026306/IODPPubs/ODP_Pubs.csv' \
+                                                       --header-line        1                                    \
+                                                       --delimiter         ','
