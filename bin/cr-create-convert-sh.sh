f1e688895ecb93bec9c93b60cfa241dbd39f6b48
Timothy Lebo <timrdf@gmail.com>
2011-06-03 13:18:27 -0400

    added spacing and name variable outputs

--- bin/cr-create-convert-sh.sh
+++ bin/cr-create-convert-sh.sh
@@ -94,6 +94,8 @@ echo "#!/bin/bash"                                                              
 echo "#"                                                                                               >> $TMP_SH
 echo "# <#> a <http://purl.org/twc/vocab/conversion/ConversionTrigger> ;"                              >> $TMP_SH
 echo "#     rdfs:seeAlso <https://github.com/timrdf/csv2rdf4lod-automation/wiki/Conversion-trigger> ." >> $TMP_SH
+echo "#"                                                                                               >> $TMP_SH
+echo "# datasetID versionID lastModDate:"                                                              >> $TMP_SH
 echo "# $datasetID $versionID ($lastModDate)"                                                          >> $TMP_SH
 
 echo "#--------------------------------------------------------------"    >> $TMP_SH
