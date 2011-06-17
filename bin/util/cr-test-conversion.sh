dc6973eee822a85e0298feda457dd24db98c1cd7
Timothy Lebo <timrdf@gmail.com>
2011-06-17 18:46:55 -0400

    if rq/ not in conversion cockpit, will use ../../rq

--- bin/util/cr-test-conversion.sh
+++ bin/util/cr-test-conversion.sh
@@ -4,25 +4,36 @@
 #
 # Run from a https://github.com/timrdf/csv2rdf4lod-automation/wiki/Conversion-cockpit
 #
-# Example for how to set up rq/:
+# Requires the following structure in an rq/ directory:
 #
 # rq
 # rq/test
 # rq/test/ask
 # rq/test/ask/absent
-# rq/test/ask/absent/9-to-7.rq
+# rq/test/ask/absent/9-to-7.rq (the filename is up to you)
 # rq/test/ask/present
-# rq/test/ask/present/0-to-2.rq
-# rq/test/ask/present/2-to-3.rq
-# rq/test/ask/present/3-to-5.rq
-# rq/test/ask/present/3-to-7.rq
-# rq/test/ask/present/5-to-1.rq
-# rq/test/ask/present/7-to-5.rq
+# rq/test/ask/present/0-to-2.rq (the filename is up to you)
+# rq/test/ask/present/2-to-3.rq (the filename is up to you)
+#
+# This script assumes a particular syntactic structure to abbreviate the query when showing execution output:
+# (use this same capitalization and indenting)
+#
+# ...
+# ASK
+# WHERE {
+#    GRAPH ?g {
+#       ...
+#    }
+# }
+#
+# Script assumes the publish/tdb/ directory is already with the data to test.
+# This can be done by running publish/bin/publish.sh
+# and then publish/bin/tdbloader-*.sh
 
 #CSV2RDF4LOD_HOME=${CSV2RDF4LOD_HOME:?"not set; source csv2rdf4lod/source-me.sh or see https://github.com/timrdf/csv2rdf4lod-automation/wiki/CSV2RDF4LOD-not-set"}
 
 if [ ${1-"."} == "--help" ]; then
-   echo "usage: `basename $0` [--verbose]"
+   echo "usage: `basename $0` [--verbose | -v]" # TODO: parameterize the rq directory.
 fi
 
 CSV2RDF4LOD_PUBLISH=true
@@ -40,13 +51,18 @@ if [ ${1-"."} == "--verbose" -o ${1-"."} == "-v" ]; then
    verbose="true"
 fi
 
+rq_dir="rq/test"
+if [ ! -d $rq_dir -a -d '../../rq/test' ]; then
+   rq_dir="../../rq/test"
+fi
+
 let "passed = 0"
 let "total  = 0"
-for rq in `find rq/test -name "*.rq"`; do 
+for rq in `find $rq_dir -name "*.rq"`; do 
 
    let "total = total + 1"
 
-   response=`tdbquery --loc publish/tdb --query $rq 2>&1 | grep -v WARN` 
+   response=`tdbquery --loc publish/tdb --query $rq 2>&1 | grep -v WARN` # TODO: parameterize the tdb directory
 
    if [[ $rq =~ rq/test/ask/present.* && $response =~ .*Yes.*   ||   $rq =~ rq/test/ask/absent.* && $response =~ .*No.* ]]; then
       passedB="true"
