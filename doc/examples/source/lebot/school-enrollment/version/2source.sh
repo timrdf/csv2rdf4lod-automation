9fde2f66ac1d2b911ae3116ac1feb2a895c4e6ab
Timothy Lebo <timrdf@gmail.com>
2011-07-08 21:18:04 -0400

    grabs school enrollment examples from google spreadsheet

---
+++ doc/examples/source/lebot/school-enrollment/version/2source.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#
+# Script to retrieve and convert a new version of the dataset.
+# See https://github.com/timrdf/csv2rdf4lod-automation/wiki/Automated-creation-of-a-new-Versioned-Dataset
+
+export CSV2RDF4LOD_CONVERT_OMIT_RAW_LAYER="true"
+google2source.sh -w 0ArTeDpS4-nUDdEZ5UFhWRm5XNXM2RWxZZ2VhaDRNbHc auto 0ArTeDpS4-nUDdDlFR0cyRTUzZE5Oblc5cWhpM3JTS3c
