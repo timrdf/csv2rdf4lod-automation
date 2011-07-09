20b82d5879ec4a25c34f394620544da7baed7a79
Timothy Lebo <timrdf@gmail.com>
2011-07-08 21:27:04 -0400

    swapped order of tables.

--- doc/examples/source/lebot/school-enrollment/version/2source.sh
+++ doc/examples/source/lebot/school-enrollment/version/2source.sh
@@ -4,4 +4,4 @@
 # See https://github.com/timrdf/csv2rdf4lod-automation/wiki/Automated-creation-of-a-new-Versioned-Dataset
 
 export CSV2RDF4LOD_CONVERT_OMIT_RAW_LAYER="true"
-google2source.sh -w 0ArTeDpS4-nUDdEZ5UFhWRm5XNXM2RWxZZ2VhaDRNbHc auto 0ArTeDpS4-nUDdDlFR0cyRTUzZE5Oblc5cWhpM3JTS3c
+google2source.sh -w 0ArTeDpS4-nUDdDlFR0cyRTUzZE5Oblc5cWhpM3JTS3c auto 0ArTeDpS4-nUDdEZ5UFhWRm5XNXM2RWxZZ2VhaDRNbHc
