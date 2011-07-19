b6b2622b159955cfd89cd4dcb0153cd8093ee93f
Timothy Lebo <timrdf@gmail.com>
2011-07-19 19:03:04 -0400

    added commands to usage to help set up cr-test...sh

--- bin/util/cr-test-conversion.sh
+++ bin/util/cr-test-conversion.sh
@@ -43,6 +43,7 @@ CSV2RDF4LOD_PUBLISH=true
 
 if [[ ! -e publish/tdb && ${#CSV2RDF4LOD_PUBLISH_TDB_DIR} == 0 ]]; then
    echo "publish/tdb does not exist and \$CSV2RDF4LOD_PUBLISH_TDB_DIR not set."
+   echo "export CSV2RDF4LOD_PUBLISH_TDB=true; publish/bin/publish.sh"
    exit 1
 fi
 
