9bcba14e18e78dd7efb79540f98ebc5c80edba55
Timothy Lebo <timrdf@gmail.com>
2011-07-27 17:24:51 -0400

    tweaking, adding links to documentation, responding to Doug uses, pushed sourceDir to every file (step towards handling mixed manual/source files and nested dirs.

--- bin/cr-create-convert-sh.sh
+++ bin/cr-create-convert-sh.sh
@@ -4,10 +4,12 @@
 # Running the created script once will produce the raw conversion; 
 # running it a second time will produce the enhanced conversion.                                                      
 #
-#        \/ source                                      
-#        |       \/ dataset                         
-#        |        |          version  
-#        |        |         \/
+#       \./ source-id                                      
+#        |
+#        |       \./ dataset-id
+#        |        |           
+#        |        |         .-- version-id
+#        |        |         |
 # source/data-gov/9/version/2010-Jun-9
 #
 # source/nci-nih-gov/popscigrid-nhis-2000-2005/version/2010-Jun-9
@@ -16,8 +18,19 @@
 #                    ^^ dataset                        ^
 #                                                      ^^ version  
 if [ $# -lt 1 ]; then
-   echo "usage: `basename $0` [-w] [--comment-character char] [--header-line row] [--delimiter delimiter] [-s sourceIdentifier] [-d datasetIdentifier] [-v versionIdentifier] a.csv [another.csv ...]\\n
-run from csv2rdf4lod/data/source/SSS/version/VVV/"
+   echo "usage: `basename $0` [-w] [--comment-character char] [--header-line row] [--delimiter delimiter] a.csv [another.csv ...]"
+   echo " (run from conversion cockpit, e.g. csv2rdf4lod/data/source/SSS/version/VVV/)"
+   echo " (see https://github.com/timrdf/csv2rdf4lod-automation/wiki/Conversion-cockpit)"
+   ecoh "parameters:"
+   echo "   -w : write the conversion trigger to disk instead of printing to stdout"
+   echo "   --comment-character : "
+   echo "   --header-line : "
+   echo "   --delimiter : "
+   echo ""
+   echo "   deprecated params:"
+   echo "   -s sourceIdentifier  : override the source-identifier."
+   echo "   -d datasetIdentifier : override the dataset-identifier."
+   echo "   -v versionIdentifier : override the version-identifier."
    exit 1
 fi
 
@@ -59,10 +72,12 @@ fi
 #
 if [ "$1" == "-s" -a $# -ge 2 ]; then
    sourceID="$2"
+   explainSourceID="Came from -s parameter to `basename $0`"
    shift 2
 else 
    back_three=`cd ../../.. 2>/dev/null && pwd`
    sourceID=`basename $back_three` # Use the names from the canonical directory structure
+   explainSourceID="Came from directory name ../../../ (see https://github.com/timrdf/csv2rdf4lod-automation/wiki/Directory-Conventions)"
 fi
 if [ $sourceID == "data.gov" ]; then
    sourceID="data-gov"
@@ -71,19 +86,23 @@ fi
 #
 if [ "$1" == "-d" -a $# -ge 2 ]; then
    datasetID="$2"
+   explainDatasetID="Came from -d parameter to `basename $0`"
    shift 2
 else 
    back_two=`cd ../.. 2>/dev/null && pwd`
    datasetID=`basename $back_two` # Use the names from the canonical directory structure
+   explainDatasetID="Came from directory name ../../ (see https://github.com/timrdf/csv2rdf4lod-automation/wiki/Directory-Conventions)"
 fi
 
 #
 if [ "$1" == "-v" -a $# -ge 2 ]; then
    versionID="$2"
+   explainVersionID="Came from -v parameter to `basename $0`"
    shift 2
 else 
    datasetDir=`pwd`
    versionID=`basename $datasetDir` # Use the names from the canonical directory structure
+   explainVersionID="Came from directory name ../ (see https://github.com/timrdf/csv2rdf4lod-automation/wiki/Directory-Conventions)"
 fi
 
 CSV2RDF4LOD_BASE_URI=${CSV2RDF4LOD_BASE_URI:?"not set; source csv2rdf4lod/source-me.sh or see https://github.com/timrdf/csv2rdf4lod-automation/wiki/CSV2RDF4LOD-not-set"}
@@ -99,27 +118,27 @@ echo "#"                                                                        
 echo "# datasetID versionID (lastModDate):"                                                                            >> $TMP_SH
 echo "# $datasetID $versionID ($lastModDate)"                                                                          >> $TMP_SH
 
-echo "#--------------------------------------------------------------"    >> $TMP_SH
-echo ""                                                                   >> $TMP_SH
+echo "#--------------------------------------------------------------"                                                 >> $TMP_SH
+echo ""                                                                                                                >> $TMP_SH
 echo 'CSV2RDF4LOD_HOME=${CSV2RDF4LOD_HOME:?"not set; source csv2rdf4lod/source-me.sh or see https://github.com/timrdf/csv2rdf4lod-automation/wiki/CSV2RDF4LOD-not-set"}' >> $TMP_SH
-echo ""                                                                   >> $TMP_SH
-echo "surrogate=\"$CSV2RDF4LOD_BASE_URI\" # Came from \$CSV2RDF4LOD_BASE_URI when `basename $0` created this script." >> $TMP_SH
-echo "sourceID=\"$sourceID\""                                             >> $TMP_SH
-echo "datasetID=\"$datasetID\""                                           >> $TMP_SH
-echo "datasetVersion=\"$versionID\"        # NO SPACES; Use curl -I -L http://www.data.gov/download/$datasetID/csv | grep "Last-Modified:" | awk '{printf("%s-%s-%s",$3,$4,$5)}'" >> $TMP_SH
-echo "versionID=\"$versionID\"             # renaming datasetVersion (deprecating datasetVersion)"       >> $TMP_SH
-echo "eID=\"1\"                             # enhancement identifier"     >> $TMP_SH
-echo "if [ \$# -ge 2 ]; then"                                             >> $TMP_SH
-echo "   if [ \$1 == \"-e\" ]; then"                                      >> $TMP_SH
-echo "     eID=\"\$2\" "                                                  >> $TMP_SH
-echo "   fi"                                                              >> $TMP_SH
-echo "fi"                                                                 >> $TMP_SH
-echo ""                                                                   >> $TMP_SH
-echo ""                                                                   >> $TMP_SH
-echo "# $1"                                                               >> $TMP_SH
-echo "sourceDir=\"`dirname "$1"`\"                  # if directly from source, 'source'; if manual manipulations of source were required, 'manual'." >> $TMP_SH
-echo "destDir=\"automatic\"                 # always 'automatic'"         >> $TMP_SH
-echo "#--------------------------------------------------------------"    >> $TMP_SH
+echo ""                                                                                                                >> $TMP_SH
+echo "# The identifiers used to name the dataset that will be converted."                                              >> $TMP_SH
+echo "#            (see https://github.com/timrdf/csv2rdf4lod-automation/wiki/Conversion-process-phase:-name)"                >> $TMP_SH
+echo "surrogate=\"$CSV2RDF4LOD_BASE_URI\" # Came from \$CSV2RDF4LOD_BASE_URI when `basename $0` created this script."  >> $TMP_SH
+echo "sourceID=\"$sourceID\"               # $explainSourceID"                                                         >> $TMP_SH
+echo "datasetID=\"$datasetID\"             # $explainDatasetID"                                                        >> $TMP_SH
+echo "datasetVersion=\"$versionID\"        # DEPRECATED"                                                               >> $TMP_SH
+echo "versionID=\"$versionID\"             # $explainVersionID renaming datasetVersion (deprecating datasetVersion)"   >> $TMP_SH
+echo "eID=\"1\"                             # enhancement identifier"                                                  >> $TMP_SH
+echo "if [[ \${1:-\".\"} == \"-e\" && \$# -ge 2 ]]; then"                                                              >> $TMP_SH
+echo "   eID=\"\$2\" # see https://github.com/timrdf/csv2rdf4lod-automation/wiki/Generating-enhancement-parameters"    >> $TMP_SH
+echo "fi"                                                                                                              >> $TMP_SH
+echo ""                                                                                                                >> $TMP_SH
+echo ""                                                                                                                >> $TMP_SH
+#echo "# $1"                                                                                                            >> $TMP_SH
+#echo "sourceDir=\"`dirname "$1"`\"                  # if converting data directly (unmodified) from source organization, 'source'; if manual manipulations were required, 'manual'." >> $TMP_SH
+echo "destDir=\"automatic\"                 # convention has led to always be 'automatic'; the directory where the converted RDF is put."       >> $TMP_SH
+echo "#--------------------------------------------------------------"                                                 >> $TMP_SH
 
 if [ $# -gt 1 ]; then
    includeDiscriminator="yes"
@@ -130,11 +149,14 @@ fi
 while [ $# -gt 0 ]; do
    artifact="$1"
 
-   echo ""                                                             >> $TMP_SH
-   echo ""                                                             >> $TMP_SH
-   echo "#-----------------------------------"                         >> $TMP_SH
-   echo "datafile=\"`basename "$artifact"`\""                          >> $TMP_SH
-   echo "data=\"\$sourceDir/\$datafile\""                              >> $TMP_SH
+   echo ""                                                                                                                  >> $TMP_SH
+   echo ""                                                                                                                  >> $TMP_SH
+   echo "#-----------------------------------"                                                                              >> $TMP_SH
+   echo "# $1"                                                                                                              >> $TMP_SH
+   echo "sourceDir=\"`dirname "$artifact"`\""                                                                               >> $TMP_SH
+   echo "datafile=\"`basename "$artifact"`\""                                                                               >> $TMP_SH
+   echo "data=\"\$sourceDir/\$datafile\""                                                                                   >> $TMP_SH
+   echo "# Bootstrap conversion parameters (see https://github.com/timrdf/csv2rdf4lod-automation/wiki/Conversion-trigger):" >> $TMP_SH
    subjectDiscriminator=""
    if [ $includeDiscriminator == "yes" ]; then
       extensionlessFilename=`basename $artifact | sed 's/^\([^\.]*\)\..*$/\1/'` # failed to cast 'ce.supersector.csv' to 'ce.supersector'
@@ -143,23 +165,24 @@ while [ $# -gt 0 ]; do
       subjectDiscriminator=`echo $extensionlessFilename | sed 's/%20/-/g; s/_/-/g' | awk '{print tolower($0)}'`
    fi
    echo "subjectDiscriminator=\"$subjectDiscriminator\"             # Additional part of URI for subjects created; must be URI-ready (e.g., no spaces)." >> $TMP_SH
-   echo "commentCharacter=\"$commentCharacter\"                 # ONLY one character; complain to http://sourceforge.net/projects/javacsv/ otherwise." >> $TMP_SH
-   echo "cellDelimiter=\"$cellDelimiter\"                   # ONLY one character; complain to http://sourceforge.net/projects/javacsv/ otherwise." >> $TMP_SH
-   echo "header=$headerRow                             # Line that header is on; only needed if not '1'. '0' means no header." >> $TMP_SH
-   echo "dataStart=                          # Line that data starts; only needed if not immediately after header." >> $TMP_SH
-   echo "repeatAboveIfEmptyCol=              # 'Fill in' value from row above for this column."                     >> $TMP_SH
-   echo "onlyIfCol=                          # Do not process if value in this column is empty"                     >> $TMP_SH
-   echo "interpretAsNull=                    # NO SPACES"              >> $TMP_SH
-   echo "dataEnd=                            # Line on which data stops; only needed if non-data bottom matter (legends, footnotes, etc)." >> $TMP_SH
-   echo "source \$CSV2RDF4LOD_HOME/bin/convert.sh"                     >> $TMP_SH
+   echo "commentCharacter=\"$commentCharacter\"                 # ONLY one character; complain to http://sourceforge.net/projects/javacsv/ otherwise."   >> $TMP_SH
+   echo "cellDelimiter=\"$cellDelimiter\"                   # ONLY one character; complain to http://sourceforge.net/projects/javacsv/ otherwise."       >> $TMP_SH
+   echo "header=$headerRow                             # Line that header is on; only needed if not '1'. '0' means no header."                           >> $TMP_SH
+   echo "dataStart=                          # Line that data starts; only needed if not immediately after header."                                      >> $TMP_SH
+   echo "repeatAboveIfEmptyCol=              # 'Fill in' value from row above for this column."                                                          >> $TMP_SH
+   echo "onlyIfCol=                          # Do not process if value in this column is empty"                                                          >> $TMP_SH
+   echo "interpretAsNull=                    # NO SPACES"                                                                                                >> $TMP_SH
+   echo "dataEnd=                            # Line on which data stops; only needed if non-data bottom matter (legends, footnotes, etc)."               >> $TMP_SH
+   echo "source \$CSV2RDF4LOD_HOME/bin/convert.sh"                                                                                                       >> $TMP_SH
 
    shift
 done
 
-echo ""                                                                >> $TMP_SH
-echo ""                                                                >> $TMP_SH
-echo "#-----------------------------------"                            >> $TMP_SH
-echo "source \$CSV2RDF4LOD_HOME/bin/convert-aggregate.sh"              >> $TMP_SH
+echo ""                                                                            >> $TMP_SH
+echo ""                                                                            >> $TMP_SH
+echo "#-----------------------------------"                                        >> $TMP_SH
+echo "source \$CSV2RDF4LOD_HOME/bin/convert-aggregate.sh"                          >> $TMP_SH
+echo "# ^^ Note, this step can also be done manually using publish/bin/publish.sh" >> $TMP_SH
 
 if [ $writeSH == "yes" ]; then
    mv $TMP_SH convert-$datasetID.sh
