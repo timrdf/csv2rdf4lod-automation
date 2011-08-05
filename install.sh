39ea730423fe031070f902457bff35d170a1f31a
Timothy Lebo <timrdf@gmail.com>
2011-08-05 19:03:57 -0400

    added --csh param"

--- install.sh
+++ install.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
+ext="sh"
+if [ "$1" == "--csh" ]; then
+   ext="csh"
+fi
+
 CSV2RDF4LOD_HOME=`pwd`
-echo "export CSV2RDF4LOD_HOME=\"$CSV2RDF4LOD_HOME\"" > my-csv2rdf4lod-source-me.sh
+echo "export CSV2RDF4LOD_HOME=\"$CSV2RDF4LOD_HOME\"" > my-csv2rdf4lod-source-me.${ext}
 echo "`basename $0`:"
-echo "   has created my-csv2rdf4lod-source-me.sh."
-echo "   has set \$CSV2RDF4LOD_HOME to $CSV2RDF4LOD_HOME in my-csv2rdf4lod-source-me.sh"
+echo "   has created my-csv2rdf4lod-source-me.${ext}."
+echo "   has set \$CSV2RDF4LOD_HOME to $CSV2RDF4LOD_HOME in my-csv2rdf4lod-source-me.${ext}"
 
 cat bin/setup.sh | grep -v "# NOTE:" >> my-csv2rdf4lod-source-me.sh
 
 if [ "$1" == "--csh" ]; then
+ # perl -pi -e
    perl -pe 's/export [^ =]*$//; s/^(\w+)="([^"]*)"/setenv \1 "\2"/; s/export (\S+)="([^"]*)"/setenv \1 "\2"/' \
          my-csv2rdf4lod-source-me.sh > my-csv2rdf4lod-source-me.csh
 elif [ "$1" == "--cygwin" ]; then
@@ -18,10 +24,10 @@ fi
 echo ""
 
 echo "~~~ What to do next: ~~~"
-echo "   'source my-csv2rdf4lod-source-me.sh' to set environment variables."
-echo "    sourcing my-csv2rdf4lod-source-me.sh must be done each time you log in, so consider adding this to your .bashrc:"
+echo "   'source my-csv2rdf4lod-source-me.${ext}' to set environment variables."
+echo "    sourcing my-csv2rdf4lod-source-me.${ext} must be done each time you log in, so consider adding this to your .bashrc:"
 echo ""
-echo "      source $CSV2RDF4LOD_HOME/my-csv2rdf4lod-source-me.sh # http://purl.org/twc/id/software/csv2rdf4lod"
+echo "      source $CSV2RDF4LOD_HOME/my-csv2rdf4lod-source-me.${ext} # http://purl.org/twc/id/software/csv2rdf4lod"
 echo ""
-echo "    use cr-vars.sh to see the environment variables that CSV2RDF4LOD uses to control execution flow."
+echo "    use cr-vars.${ext} to see the environment variables that CSV2RDF4LOD uses to control execution flow."
 mv install.sh bin/
