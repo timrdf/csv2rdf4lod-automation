5d72d9e23fdf11396d1c6d35d3ae44f50a43e009
Timothy Lebo <timrdf@gmail.com>
2011-08-06 13:36:10 -0400

    wrapping bummper lanes for rapper

---
+++ bin/util/too-big-for-rapper.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# https://github.com/timrdf/csv2rdf4lod-automation/blob/master/bin/util/too-big-for-rapper.sh
+# https://github.com/timrdf/csv2rdf4lod-automation/wiki/Dealing-with-rapper%27s-2GB-limitation
+
+if [ $# -lt 1 ]; then
+   echo "usage: `basename $0` a.ttl b.ttl ..."
+   exit 1
+fi
+
+too_big="no"
+while [ $# -gt 0 ];do
+   ttl="$1"
+   for big in `find \`dirname $ttl\` -size +1900M -name \`basename $ttl\``; do
+      too_big="yes"
+   done
+   shift
+done
