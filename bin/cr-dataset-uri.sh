2965bcef30a3fabc498d0ff9e0c93d663eccf2fe
Timothy Lebo <timrdf@gmail.com>
2011-08-06 20:18:23 -0400

    encapsulating error messages.

--- bin/cr-dataset-uri.sh
+++ bin/cr-dataset-uri.sh
@@ -5,7 +5,7 @@
 CSV2RDF4LOD_HOME=${CSV2RDF4LOD_HOME:?"not set; source csv2rdf4lod/source-me.sh or see https://github.com/timrdf/csv2rdf4lod-automation/wiki/CSV2RDF4LOD-not-set"}
 
 ACCEPTABLE_PWDs="cr:directory-of-versions cr:conversion-cockpit"
-if [ `is-pwd-a.sh $ACCEPTABLE_PWDs` != "yes" ]; then
+if [ `${CSV2RDF4LOD_HOME}/bin/util/is-pwd-a.sh $ACCEPTABLE_PWDs` != "yes" ]; then
    ${CSV2RDF4LOD_HOME}/bin/util/pwd-not-a.sh $ACCEPTABLE_PWDs
    exit 1
 fi
@@ -20,7 +20,9 @@ end="" # Period
 if [ ${1:-"."} == 'void' ]; then
    prefixDef="@prefix conversion: <http://purl.org/twc/vocab/conversion/> ."
    isabstract="    a conversion:AbstractDataset;"
-   isversioned="    a conversion:VersionedDataset;"
+   if [ `${CSV2RDF4LOD_HOME}/bin/util/is-pwd-a.sh cr:conversion-cockpit` == "yes" ]; then
+      isversioned="    a conversion:VersionedDataset;"
+   fi
    prefix="conversion:"
    a1="<"
    a2=">"
@@ -56,10 +58,12 @@ echo "    ${prefix}source_identifier     ${q1}$sourceID${q2}"
 echo "    ${prefix}dataset_identifier    ${q1}$datasetID${q2}"                                    
 echo $end
 echo
-echo "$a1$CSV2RDF4LOD_BASE_URI/source/$sourceID/dataset/$datasetID/version/$versionID$a2"
-echo      $isversioned
-echo "    ${prefix}base_uri              ${q1}$base_uri${q2}"
-echo "    ${prefix}source_identifier     ${q1}$sourceID${q2}"                                        
-echo "    ${prefix}dataset_identifier    ${q1}$datasetID${q2}"                                    
-echo "    ${prefix}version_identifier    ${q1}$versionID${q2}"
-echo $end 
+if [ `${CSV2RDF4LOD_HOME}/bin/util/is-pwd-a.sh cr:conversion-cockpit` == "yes" ]; then
+   echo "$a1$CSV2RDF4LOD_BASE_URI/source/$sourceID/dataset/$datasetID/version/$versionID$a2"
+   echo      $isversioned
+   echo "    ${prefix}base_uri              ${q1}$base_uri${q2}"
+   echo "    ${prefix}source_identifier     ${q1}$sourceID${q2}"                                        
+   echo "    ${prefix}dataset_identifier    ${q1}$datasetID${q2}"                                    
+   echo "    ${prefix}version_identifier    ${q1}$versionID${q2}"
+   echo $end 
+fi
