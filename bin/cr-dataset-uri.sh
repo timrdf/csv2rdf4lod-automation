b06d5018bbc673e1606513e00b6a6bfab4ecc92f
Timothy Lebo <timrdf@gmail.com>
2011-08-06 20:27:06 -0400

    encapsulating directory conventions

--- bin/cr-dataset-uri.sh
+++ bin/cr-dataset-uri.sh
@@ -4,7 +4,7 @@
 
 CSV2RDF4LOD_HOME=${CSV2RDF4LOD_HOME:?"not set; source csv2rdf4lod/source-me.sh or see https://github.com/timrdf/csv2rdf4lod-automation/wiki/CSV2RDF4LOD-not-set"}
 
-ACCEPTABLE_PWDs="cr:directory-of-versions cr:conversion-cockpit"
+ACCEPTABLE_PWDs="cr:dataset cr:directory-of-versions cr:conversion-cockpit"
 if [ `${CSV2RDF4LOD_HOME}/bin/util/is-pwd-a.sh $ACCEPTABLE_PWDs` != "yes" ]; then
    ${CSV2RDF4LOD_HOME}/bin/util/pwd-not-a.sh $ACCEPTABLE_PWDs
    exit 1
