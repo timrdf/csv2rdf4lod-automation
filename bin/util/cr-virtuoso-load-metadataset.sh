26e757ac70faaab8d6d52b415e5aeba74a91e56d
Timothy Lebo <timrdf@gmail.com>
2011-08-08 23:59:13 -0400

    generalizing beyond sam

--- bin/util/cr-virtuoso-load-metadataset.sh
+++ bin/util/cr-virtuoso-load-metadataset.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 # 
-# Usage: logd-load-metadata-graph.sh [-w] [-ng http://named-graph-for-endpoint] [source-id dataset-id]*
+# Usage: cr-virtuoso-load-metadataset.sh [-w] [-ng http://named-graph-for-endpoint] [source-id dataset-id]*
 # See https://github.com/timrdf/csv2rdf4lod-automation/wiki/Aggregating-subsets-of-converted-datasets
+# https://github.com/timrdf/csv2rdf4lod-automation/blob/master/bin/util/cr-virtuoso-load-metadataset.sh
+
+CSV2RDF4LOD_HOME=${CSV2RDF4LOD_HOME:?"not set; source csv2rdf4lod/source-me.sh or see https://github.com/timrdf/csv2rdf4lod-automation/wiki/CSV2RDF4LOD-not-set"}
+
+ACCEPTABLE_PWDs="cr:data-root"
+if [ `${CSV2RDF4LOD_HOME}/bin/util/is-pwd-a.sh $ACCEPTABLE_PWDs` != "yes" ]; then
+   ${CSV2RDF4LOD_HOME}/bin/util/pwd-not-a.sh $ACCEPTABLE_PWDs
+   exit 1
+   # cr:data-root cr:source cr:directory-of-datasets cr:dataset cr:directory-of-versions cr:conversion-cockpit
+fi
 
 echo "------------------- `basename $0` --------------------"
 dryRun="true"
