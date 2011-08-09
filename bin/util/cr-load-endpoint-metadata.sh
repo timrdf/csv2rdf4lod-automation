62bd53af87ab0b14e2efacb00d295642b77db653
Timothy Lebo <timrdf@gmail.com>
2011-08-09 04:31:17 -0400

    sudo

--- bin/util/cr-load-endpoint-metadata.sh
+++ bin/util/cr-load-endpoint-metadata.sh
@@ -99,7 +99,7 @@ pushd ${CSV2RDF4LOD_PUBLISH_LOD_MATERIALIZATION_WWW_ROOT}/query
    fi
    echo
    echo "cache-queries.sh start date time:"`${CSV2RDF4LOD_HOME}/bin/util/dateInXSDDateTime.sh` | tee -a $log_file
-   for query in `find . -name *.rq -o -name *.sparql`; do
+   for query in `find . -name "*.rq$" -o -name "*.sparql$"`; do
       $sudo cache-queries.sh ${CSV2RDF4LOD_PUBLISH_VIRTUOSO_SPARQL_ENDPOINT} -o sparql gvds xml csv -q $query
    done
    # -o sparql gvds xml
