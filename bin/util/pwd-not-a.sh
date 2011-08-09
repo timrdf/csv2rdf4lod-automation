48ec189715e20707050c99d57711aea6b695c58e
Timothy Lebo <timrdf@gmail.com>
2011-08-09 01:08:54 -0400

    added case for cr:source

--- bin/util/pwd-not-a.sh
+++ bin/util/pwd-not-a.sh
@@ -15,33 +15,28 @@ whatever="/srv/myPROJECT"
 while [[ $# -ge 1 ]]; do
    if   [[ $1 == "cr:directory-of-sources" || $1 == "cr:data-root"          ]]; then
       echo ""
-      echo "  Working directory does not appear to be a data root."
-      echo "  You can run this from a data root"
+      echo "  Working directory does not appear to be a data root. You can run this from a data root."
       echo "  (e.g. \$whatever/source/)."
    elif [[ $1 == "cr:source"                                                ]]; then
       echo ""
-      echo "  Working directory does not appear to be a source."
-      echo "  You can run this from a source"
+      echo "  Working directory does not appear to be a source. You can run this from a source."
       echo "  (e.g. \$whatever/source/mySOURCE)."
    elif [[ $1 == "cr:directory-of-datasets"                                 ]]; then
       echo ""
-      echo "  Working directory does not appear to be a directory of datasets."
-      echo "  You can run this from a directory of datasets"
+      echo "  Working directory does not appear to be a directory of datasets. You can run this from a directory of datasets."
       echo "  (e.g. \$whatever/source/mySOURCE/)."
    elif [[ $1 == "cr:dataset"                                               ]]; then
       echo ""
-      echo "  Working directory does not appear to be a dataset."
-      echo "  You can run this from"
+      echo "  Working directory does not appear to be a dataset. You can run this from a dataset."
       echo "  (e.g. \$whatever/source/mySOURCE/myDATASET/)."
    elif [[ $1 == "cr:directory-of-versions"                                 ]]; then
       echo ""
-      echo "  Working directory does not appear to be a directory of versions."
-      echo "  You can run this from a directory of versions"
+      echo "  Working directory does not appear to be a directory of versions. You can run this from a directory of versions."
       echo "  (e.g. \$whatever/source/mySOURCE/myDATASET/version/)."
    elif [[ $1 == "cr:version"              || $1 == "cr:conversion-cockpit" ]]; then
       echo ""
       echo "  Working directory does not appear to be a conversion cockpit."
-      echo "  You can run this from a conversion cockpit"
+      echo "  You can run this from a conversion cockpit."
       echo "  (e.g. \$whatever/source/mySOURCE/myDATASET/version/VVV/)."
    elif [[ "$1" == "cr:bone" || "$1" == "." ]]; then
       echo "  todo $1"
