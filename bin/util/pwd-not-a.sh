6262b6dd58d9afafcd6b521ddb05d4c6b3b54200
Timothy Lebo <timrdf@gmail.com>
2011-08-09 01:06:33 -0400

    added case for cr:source

--- bin/util/pwd-not-a.sh
+++ bin/util/pwd-not-a.sh
@@ -16,27 +16,33 @@ while [[ $# -ge 1 ]]; do
    if   [[ $1 == "cr:directory-of-sources" || $1 == "cr:data-root"          ]]; then
       echo ""
       echo "  Working directory does not appear to be a data root."
-      echo "  You can run this from a data root (e.g. \$whatever/source/)."
+      echo "  You can run this from a data root"
+      echo "  (e.g. \$whatever/source/)."
    elif [[ $1 == "cr:source"                                                ]]; then
       echo ""
       echo "  Working directory does not appear to be a source."
-      echo "  You can run this from a source (e.g. \$whatever/source/mySOURCE)."
+      echo "  You can run this from a source"
+      echo "  (e.g. \$whatever/source/mySOURCE)."
    elif [[ $1 == "cr:directory-of-datasets"                                 ]]; then
       echo ""
       echo "  Working directory does not appear to be a directory of datasets."
-      echo "  You can run this from a directory of datasets (e.g. \$whatever/source/mySOURCE/)."
+      echo "  You can run this from a directory of datasets"
+      echo "  (e.g. \$whatever/source/mySOURCE/)."
    elif [[ $1 == "cr:dataset"                                               ]]; then
       echo ""
       echo "  Working directory does not appear to be a dataset."
-      echo "  You can run this from (e.g. \$whatever/source/mySOURCE/myDATASET/)."
+      echo "  You can run this from"
+      echo "  (e.g. \$whatever/source/mySOURCE/myDATASET/)."
    elif [[ $1 == "cr:directory-of-versions"                                 ]]; then
       echo ""
       echo "  Working directory does not appear to be a directory of versions."
-      echo "  You can run this from a directory of versions (e.g. \$whatever/source/mySOURCE/myDATASET/version/)."
+      echo "  You can run this from a directory of versions"
+      echo "  (e.g. \$whatever/source/mySOURCE/myDATASET/version/)."
    elif [[ $1 == "cr:version"              || $1 == "cr:conversion-cockpit" ]]; then
       echo ""
       echo "  Working directory does not appear to be a conversion cockpit."
-      echo "  You can run this from a conversion cockpit (e.g. \$whatever/source/mySOURCE/myDATASET/version/VVV/)."
+      echo "  You can run this from a conversion cockpit"
+      echo "(e.g. \$whatever/source/mySOURCE/myDATASET/version/VVV/)."
    elif [[ "$1" == "cr:bone" || "$1" == "." ]]; then
       echo "  todo $1"
    else
