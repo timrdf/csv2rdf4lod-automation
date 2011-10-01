661e6125adb71acda37a81af38282997131899cd
Timothy Lebo <timrdf@gmail.com>
2011-10-01 16:28:00 -0400

    help finding where envvars are set

--- bin/util/cr-where-was-envvar-set.sh
+++ bin/util/cr-where-was-envvar-set.sh
@@ -27,8 +27,9 @@ if [[ $# -gt 0 && "$1" != "--only" ]]; then
    shift
 fi
 
+omit=""
 if [[ "$1" == "--only" ]]; then
-   omit=`echo $var | awk '{print "| grep -v \""substr($0,length($0)-2,3)"_\""}'` 
+   omit=`echo $var | awk '{print "\""substr($0,length($0)-2,3)"_\""}'` 
 fi
 
 if [[ ! -e $rc ]]; then
@@ -40,5 +41,9 @@ fi
 
 for sourceme in `grep "^source .*csv2rdf4lod-source-me*" $rc | awk '{print $2}'`; do 
    echo grep -H "^export $var" $sourceme $omit
-   grep -H "\"^export $var\"" $sourceme $omit
+   if [[ ! ${#omit} ]]; then
+      grep -H "\"^export $var\"" $sourceme
+   else
+      grep -H "\"^export $var\"" $sourceme | grep -v $omit
+   fi
 done
