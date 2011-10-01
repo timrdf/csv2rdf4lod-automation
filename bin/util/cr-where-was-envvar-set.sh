94c73324f7fe1ba45f87883302ca1becc0141783
Timothy Lebo <timrdf@gmail.com>
2011-10-01 16:25:03 -0400

    help finding where envvars are set

--- bin/util/cr-where-was-envvar-set.sh
+++ bin/util/cr-where-was-envvar-set.sh
@@ -3,11 +3,12 @@
 # https://github.com/timrdf/csv2rdf4lod-automation/blob/master/bin/util/cr-where-was-envvar-set.sh
 
 if [[ "$1" == "--help" || "$1" == "-h" ]]; then
-   echo "usage: `basename $0` [-rc ~/.bashrc] [CSV2RDF4LOD_var]"
-   echo "  -rc - the rc (.bashrc, .login, etc.) file used to source all csv2rdf4lod-source-mes"
-   echo "  [CSV2RDF4LOD_var] - a CSV2RDF4LOD_ environment variable name."
+   echo "usage: `basename $0` [-rc ~/.bashrc] [CSV2RDF4LOD_var] [--only]"
+   echo "                -rc : the rc (.bashrc, .login, etc.) file used to source all csv2rdf4lod-source-mes"
+   echo "  [CSV2RDF4LOD_var] : a CSV2RDF4LOD_ environment variable name."
    echo "                      All variables are listed by running cr-vars.sh"
    echo "                      If not specified, defaults to CSV2RDF4LOD_HOME"
+   echo "             --only : omit the CSV2RDF4LOD_ variables that are more specific than the one specified."
    echo 
    echo "see https://github.com/timrdf/csv2rdf4lod-automation/wiki/CSV2RDF4LOD-environment-variables"
    echo "    https://github.com/timrdf/csv2rdf4lod-automation/wiki/Script:-source-me.sh"
@@ -21,9 +22,13 @@ if [[ "$1" == "-rc" && $# -ge 2 ]]; then
 fi
 
 var="CSV2RDF4LOD_HOME"
-if [[ $# -gt 0 ]]; then
+if [[ $# -gt 0 && "$1" != "--only" ]]; then
    var="$1"
    shift
+fi
+
+if [[ "$1" == "--only" ]]; then
+   omit=`echo $var | awk '{print "| grep -v"substr($0,length($0)-2,3)"_"}'` 
 fi
 
 if [[ ! -e $rc ]]; then
@@ -34,5 +39,6 @@ if [[ ! -e $rc ]]; then
 fi
 
 for sourceme in `grep "^source .*csv2rdf4lod-source-me*" $rc | awk '{print $2}'`; do 
-   grep -H "^export $var" $sourceme
+   echo grep -H "^export $var" $sourceme $omit
+   grep -H "^export $var" $sourceme $omit
 done
