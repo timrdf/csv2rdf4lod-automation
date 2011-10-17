c8dc0bd78f7d6a25f59d3d04b79e38e7e8a0f4a7
Timothy Lebo <timrdf@gmail.com>
2011-10-17 09:36:27 -0400

    added retreive.sh - automated creation.

---
+++ doc/examples/source/iodp-org/scientific-ocean-drilling-repository-dspace/version/retrieve.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+#3> @prefix doap:    <http://usefulinc.com/ns/doap#> .
+#3> @prefix dcterms: <http://purl.org/dc/terms/> .
+#3> @prefix rdfs:    <http://www.w3.org/2000/01/rdf-schema#> .
+#3> 
+#3> <#>
+#3>    a doap:Project; 
+#3>    dcterms:description 
+#3>      "Script to retrieve and convert a new version of the dataset.";
+#3>    rdfs:seeAlso 
+#3>      <https://github.com/timrdf/csv2rdf4lod-automation/wiki/Automated-creation-of-a-new-Versioned-Dataset>;
+#3> .
+
+export CSV2RDF4LOD_CONVERT_OMIT_RAW_LAYER="true"
+$CSV2RDF4LOD_HOME/bin/cr-create-versioned-dataset-dir.sh cr:auto                                               \
+                                                        'https://raw.github.com/timrdf/csv2rdf4lod-automation/master/doc/examples/source/iodp-org/scientific-ocean-drilling-repository-dspace/version/2011-Oct-17/source/sandboxMeta.csv' \
+                                                       --comment-character '#'                                 \
+                                                       --header-line        1                                  \
+                                                       --delimiter         ','
