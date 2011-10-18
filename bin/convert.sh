de04af89d92dc00d35eac7a77d2fab0683a71e7d
Timothy Lebo <timrdf@gmail.com>
2011-10-18 10:06:13 -0400

    backfilling the java dependencies into the dynamic construction of CLASSPATH

--- bin/convert.sh
+++ bin/convert.sh
@@ -15,8 +15,15 @@
 CSV2RDF4LOD_HOME=${CSV2RDF4LOD_HOME:?"not set; source csv2rdf4lod/source-me.sh or see https://github.com/timrdf/csv2rdf4lod-automation/wiki/CSV2RDF4LOD-not-set"}
 
 # Java dependencies; relative to $CSV2RDF4LOD_HOME
-for jar in  lib/joda-time-2.0/joda-time-2.0.jar \
-            bin/dup/datadigest-1.0-SNAPSHOT.jar ; do
+for jar in                                                             \
+           bin/lib/javacsv2.0/javacsv.jar                              \
+           bin/dup/openrdf-sesame-2.3.1-onejar.jar                     \
+           bin/dup/slf4j-api-1.5.6.jar                                 \
+           bin/dup/slf4j-nop-1.5.6.jar                                 \
+           lib/joda-time-2.0/joda-time-2.0.jar                         \
+           bin/dup/datadigest-1.0-SNAPSHOT.jar                         \
+           bin/lib/commons-validator-1.3.1/commons-validator-1.3.1.jar \
+           bin/dup/csv2rdf4lod.jar ; do
    if [[ $CLASSPATH != *`basename $jar`* ]]; then
       if [ ${CSV2RDF4LOD_CONVERT_DEBUG_LEVEL:="."} == "fine" ]; then
          echo "`basename $jar` not in classpath; adding $CSV2RDF4LOD_HOME/$jar"
