0fdfb3eabb3f6758a5e5fc876da6f968a41803b9
Timothy Lebo <timrdf@gmail.com>
2011-11-11 16:03:58 -0500

    utils

---
+++ bin/source-id.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#
+# https://github.com/timrdf/csv2rdf4lod-automation/wiki/Conversion-process-phase:-name
+# https://github.com/timrdf/csv2rdf4lod-automation/wiki/Directory-Conventions
+
+CSV2RDF4LOD_HOME=${CSV2RDF4LOD_HOME:?"not set; source my-csv2rdf4lod-source-me.sh or see https://github.com/timrdf/csv2rdf4lod-automation/wiki/CSV2RDF4LOD-not-set"}
---
+++ bin/util/cr-default-prefixes.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# perl -pi -e 's/.prefix/\nprefix/g' if you want to pipe to a file.
+
+if [[ "$1" == "--help" ]]; then
+   echo "usage: `basename $0` [--turtle] || --sparql]"
+   echo "  --turtle: return turtle syntax (default is sparql)"
+   exit 1
+fi
+
+if [[ "$1" == "--turtle" ]]; then
+   $0 | awk '$0~"prefix" {printf("@%s .\n",$0)}'
+   echo
+   # curl http://prefix.cc/rdfs,void,conversion.file.ttl
+else
+   echo "prefix rdf:        <http://www.w3.org/1999/02/22-rdf-syntax-ns#>"
+   echo "prefix rdfs:       <http://www.w3.org/2000/01/rdf-schema#>"
+   echo "prefix owl:        <http://www.w3.org/2002/07/owl#>"
+   echo "prefix dcterms:    <http://purl.org/dc/terms/>"
+   echo "prefix foaf:       <http://xmlns.com/foaf/0.1/>"
+   echo "prefix skos:       <http://www.w3.org/2004/02/skos/core#>"
+   echo "prefix sioc:       <http://rdfs.org/sioc/ns#>"
+   echo "prefix void:       <http://rdfs.org/ns/void#>"             
+   echo "prefix conversion: <http://purl.org/twc/vocab/conversion/>"
+   echo "prefix twi:        <http://tw.rpi.edu/instances/>"
+   echo
+   # curl http://prefix.cc/rdfs,void,conversion.file.sparql | grep PREFIX
+fi
