970b38eac092f783dab80ba34e438fe7f0b5bcd0
Timothy Lebo <timrdf@gmail.com>
2011-11-12 16:38:25 -0500

    publishing params to endpoint

--- bin/cr-publish-params-to-endpoint.sh
+++ bin/cr-publish-params-to-endpoint.sh
@@ -21,6 +21,17 @@
 #    todo:
 #       deletes publish/* (not automatic/*) and runs ./convert-1263.sh in all version directories.
 
+CSV2RDF4LOD_HOME=${CSV2RDF4LOD_HOME:?"not set; source csv2rdf4lod/source-me.sh or see https://github.com/timrdf/csv2rdf4lod-automation/wiki/CSV2RDF4LOD-not-set"}
+
+# cr:data-root cr:source cr:directory-of-datasets cr:dataset cr:directory-of-versions cr:conversion-cockpit
+#ACCEPTABLE_PWDs="cr:data-root"
+#if [ `${CSV2RDF4LOD_HOME}/bin/util/is-pwd-a.sh $ACCEPTABLE_PWDs` != "yes" ]; then
+#   ${CSV2RDF4LOD_HOME}/bin/util/pwd-not-a.sh $ACCEPTABLE_PWDs
+#   exit 1
+#fi
+
+TEMP="_"`basename $0``date +%s`_$$.tmp
+
 back_one=`cd .. 2>/dev/null && pwd`
 back_zero=`pwd`
 ANCHOR_SHOULD_BE_SOURCE=`basename $back_one`
@@ -37,6 +48,8 @@ else
    numDatasets="one"      
    namedGraph="http://purl.org/twc/vocab/conversion/ConversionProcess"
 fi
+
+echo $numDatasets
 
 if [ $# -lt 1 ]; then
    echo "usage: `basename $0` [-n] <named_graph_URI | auto | .>"
