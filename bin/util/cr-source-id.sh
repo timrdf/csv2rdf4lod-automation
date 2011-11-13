d16ac70316a385e19718aa2b03de8ec24ad0b58d
Timothy Lebo <timrdf@gmail.com>
2011-11-13 14:34:33 -0500

    cleaner source-id

---
+++ bin/util/cr-source-id.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Return the source identifier, based on
+# https://github.com/timrdf/csv2rdf4lod-automation/wiki/Directory-Conventions
+${CSV2RDF4LOD_HOME}/bin/util/is-pwd-a.sh cr:bone --id-of source
