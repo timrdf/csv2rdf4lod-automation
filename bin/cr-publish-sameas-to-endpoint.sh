fa9b7d6db1b4fc9f159d94f679fc81c2ef780c6d
Timothy Lebo <timrdf@gmail.com>
2011-11-13 15:07:39 -0500

    cleaned up. added --target --clear-graph cr:auto

--- bin/cr-publish-sameas-to-endpoint.sh
+++ bin/cr-publish-sameas-to-endpoint.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 #
+# https://github.com/timrdf/csv2rdf4lod-automation/blob/master/bin/cr-publish-sameas-to-endpoint.sh
+#
 # See also:
-# https://github.com/timrdf/csv2rdf4lod-automation/wiki/Aggregating-subsets-of-converted-datasets
+#    https://github.com/timrdf/csv2rdf4lod-automation/wiki/Aggregating-subsets-of-converted-datasets
 #
 # Usage:
 #    (pwd: source/DDD, e.g. source/data-gov):
@@ -21,30 +23,31 @@
 #    todo:
 #       deletes publish/* (not automatic/*) and runs ./convert-1263.sh in all version directories.
 
-back_one=`cd .. 2>/dev/null && pwd`
-back_zero=`pwd`
-ANCHOR_SHOULD_BE_SOURCE=`basename $back_one`
-if [ $ANCHOR_SHOULD_BE_SOURCE != "source" ]; then
-   if [ `basename $back_zero` == "source" ]; then
-      numDatasets="all"      
-      namedGraph="http://purl.org/twc/vocab/conversion/SameAsDataset"
-   else
-      echo "  Working directory does not appear to be a SOURCE directory."
-      echo "  Run `basename $0` from a SOURCE directory (e.g. csv2rdf4lod/data/source/SOURCE/)"
-      exit 1
-   fi
-else
-   numDatasets="one"      
-   namedGraph="http://purl.org/twc/vocab/conversion/SameAsDataset"
+CSV2RDF4LOD_HOME=${CSV2RDF4LOD_HOME:?"not set; source csv2rdf4lod/source-me.sh or see https://github.com/timrdf/csv2rdf4lod-automation/wiki/CSV2RDF4LOD-not-set"}
+
+# cr:data-root cr:source cr:directory-of-datasets cr:dataset cr:directory-of-versions cr:conversion-cockpit
+ACCEPTABLE_PWDs="cr:data-root cr:source"
+if [ `${CSV2RDF4LOD_HOME}/bin/util/is-pwd-a.sh $ACCEPTABLE_PWDs` != "yes" ]; then
+   ${CSV2RDF4LOD_HOME}/bin/util/pwd-not-a.sh $ACCEPTABLE_PWDs
+   exit 1
 fi
 
-if [ $# -lt 1 ]; then
-   echo "usage: `basename $0` [-n] <named_graph_URI | auto | .>"
+TEMP="_"`basename $0``date +%s`_$$.tmp
+
+graphName="http://purl.org/twc/vocab/conversion/SameAsDataset"
+
+if [[ "$1" == "--help" || $# -lt 1 ]]; then
+   echo "usage: `basename $0` [--target] [-n] [--clear-graph] <named_graph_URI | cr:auto | .>"
    echo ""
-   echo "Find all void subset ttl files and put them into a named graph on a virtuoso sparql endpoint."
+   echo "    Find all void subset ttl files and put them into a named graph on a virtuoso sparql endpoint."
    echo ""
-   echo "  auto - use named graph $namedGraph"
-   echo "  .    - print to stdout"
+   echo "         --target : return the name of graph that will be loaded; then quit."
+   echo "               -n : perform dry run only; do not load named graph."
+   echo "    --clear-graph : clear the named graph."
+   echo
+   echo "  named_graph_URI : use graph name given"
+   echo "          cr:auto : use graph name $namedGraph"
+   echo "                . : print to stdout (to not put in graph)"
    exit 1
 fi
 
@@ -58,62 +61,52 @@ if [ "$1" == "-n" ]; then
 fi
 
 if [ $# -lt 1 ]; then
-   echo "usage: `basename $0` [-n] <named graph URI | auto>"
+   $0 --help
    exit 1
 fi
 
-if [ $# -gt 0 -a "$1" != "auto" ]; then
-   namedGraph="$1"
+if [ "$1" == "--clear-graph" ]; then
+   echo ""
+   echo "Deleting $graphName"                                         >&2
+   echo  "  ${CSV2RDF4LOD_HOME}/bin/util/virtuoso/vdelete $graphName" >&2
+   if [ "$dryRun" != "true" -a $graphName != "." ]; then
+      ${CSV2RDF4LOD_HOME}/bin/util/virtuoso/vdelete $graphName 
+   fi
+   shift
+fi
+
+if [ "$1" != "cr:auto" ]; then
+   graphName="$1"
    shift 
 fi
 
-CSV2RDF4LOD_HOME=${CSV2RDF4LOD_HOME:?"must be set. source csv2rdf4lod/source-me.sh."}
-CSV2RDF4LOD_BASE_URI=${CSV2RDF4LOD_BASE_URI:?"must be set. source csv2rdf4lod/source-me.sh."}
-
-TEMP_sameas="_"`basename $0``date +%s`_$$.tmp
-
-assudo="sudo"
-if [ `whoami` == "root" ]; then
-   assudo=""
+echo "Finding all SameAs from `pwd`. Will populate into $graphName" >&2
+if [ `is-pwd-a.sh cr:source` == "yes" ]; then
+   sames=`find   */version/*/publish -name "*sameas.nt" | xargs du -s | sort -nr | awk '$2!="total"{print $2}'`
+elif [ `is-pwd-a.sh cr:data-root` == "yes" ]; then
+   sames=`find */*/version/*/publish/ -name "*sameas.nt" | xargs du -s | sort -nr | awk '$2!="total"{print $2}'`
 fi
 
-echo "Finding all SameAs from `pwd`. Will populate into $namedGraph" >&2
-if [ $numDatasets == "one" ]; then
-   sames=`$assudo find */version/*/publish -name "*sameas.nt" | xargs du -s | sort -nr | awk '$2!="total"{print $2}'`
-else
-   sames=`$assudo find */*/version/*/publish/ -name "*sameas.nt" | xargs du -s | sort -nr | awk '$2!="total"{print $2}'`
-fi
-
-for same in $sames
-do
+for same in $sames; do
    count=`wc $same | awk '{print $1}'`
    echo "$count . $same" >&2
-   cat $same >> $TEMP_sameas
-   #if [ ${dryRun:-"."} != "true" ]; then
-   #   rapper -i turtle -o ntriples $same >> $TEMP_sameas
+   cat $same >> $TEMP
+   #if [ "$dryRun" != "true" ]; then
+   #   rapper -i turtle -o ntriples $same >> $TEMP
    #fi
 done
 
 echo ""
-echo "Deleting $namedGraph"                                         >&2
-echo  "  ${CSV2RDF4LOD_HOME}/bin/util/virtuoso/vdelete $namedGraph" >&2
-if [ ${dryRun:-"."} != "true" -a $namedGraph != "." ]; then
-   # @deprecated: $assudo /opt/virtuoso/scripts/vdelete               $namedGraph 
-   ${CSV2RDF4LOD_HOME}/bin/util/virtuoso/vdelete $namedGraph 
+echo "Loading sameas into $graphName"                                    >&2
+echo "  ${CSV2RDF4LOD_HOME}/bin/util/virtuoso/vload nt $TEMP $graphName" >&2
+if [ ${dryRun:-"."} != "true" -a $graphName != "." ]; then
+   ${CSV2RDF4LOD_HOME}/bin/util/virtuoso/vload nt $TEMP $graphName
 fi
 
-echo ""
-echo "Loading sameas into $namedGraph"                                           >&2
-echo "  ${CSV2RDF4LOD_HOME}/bin/util/virtuoso/vload nt $TEMP_sameas $namedGraph" >&2
-if [ ${dryRun:-"."} != "true" -a $namedGraph != "." ]; then
-   # @deprecated: $assudo /opt/virtuoso/scripts/vload   nt $TEMP_sameas $namedGraph
-   ${CSV2RDF4LOD_HOME}/bin/util/virtuoso/vload nt $TEMP_sameas $namedGraph
-fi
-
-if [ -e $TEMP_sameas ]; then
-   if [ $namedGraph == "." ]; then
+if [ -e $TEMP ]; then
+   if [ $graphName == "." ]; then
       echo "dumping to stdout" >&2      
-      cat $TEMP_sameas
+      cat $TEMP
    fi
-   rm $TEMP_sameas 
+   rm $TEMP 
 fi
