ac6a6374a4663ab4671e8bda059a1ec070e126a9
Timothy Lebo <timrdf@gmail.com>
2012-01-08 04:20:27 -0500

    latest version only

--- bin/cr-pull-conversion-triggers.sh
+++ bin/cr-pull-conversion-triggers.sh
@@ -19,7 +19,7 @@
 #       deletes publish/* (not automatic/*) and runs ./convert-1263.sh in all version directories.
 
 if [ "$1" == "--help" ]; then
-   echo "usage: `basename $0` [-w] [--layer {raw,e1,e2,...,cr:ALL}] [-sourceDir {source,manual}] <cr:ALL | datasetID [datasetID]*>"
+   echo "usage: `basename $0` [-w] [--layer {raw,e1,e2,...,cr:ALL}] [--latest-version-only] [-sourceDir {source,manual}] <cr:ALL | datasetID [datasetID]*>"
    echo ""
 #   echo "Remove everything in:"
 #   echo " source/SSS/DDD/version/VVV/automatic/* and"
@@ -28,10 +28,11 @@ if [ "$1" == "--help" ]; then
    echo "Rerun raw and all enhancement conversions using"
    echo " source/SSS/DDD/version/VVV/automatic/convert-DDD.sh if it is present."
    echo ""
-   echo "   -w:         prevent dry run; actually run scripts."
-   echo "   --layer:    conversion identifier to publish (raw, e1, e2, ...) (if not specified, converts all layers.)"
-   echo "   -sourceDir: if specified, replace source/SSS/DDD/version/VVV/automatic/convert-DDD.sh "
-   echo "               with a newly-generated convert-DDD.sh for CSVs in the {source,manual} directory."
+   echo "   -w:                    prevent dry run; actually run scripts."
+   echo "   --latest-version-only: only convert the latest version for each dataset (uses directory modification date)."
+   echo "   --layer:               conversion identifier to publish (raw, e1, e2, ...) (if not specified, converts all layers.)"
+   echo "   -sourceDir:            if specified, replace source/SSS/DDD/version/VVV/automatic/convert-DDD.sh "
+   echo "                          with a newly-generated convert-DDD.sh for CSVs in the {source,manual} directory."
    echo ""
    echo "Invoke from source/SSS to re-convert the given conversion layers for the given dataset-ids."
    echo "Invoke from source/    to re-convert the given conversion layers for the given dataset-ids from all sources."
@@ -63,6 +64,12 @@ focusLayer="cr:ALL"
 if [ "$1" == "--layer" ]; then
    focusLayer="$2"
    shift 2
+fi
+
+latest_version_only=""
+if [ "$1" == "--latest-version-only" ]; then
+   latest_version_only="| tail -1"
+   shift
 fi
 
 # This was needed when first transitioning to the csv2rdf4lod file organization,
@@ -99,7 +106,7 @@ elif [[ `is-pwd-a.sh cr:source` == "yes" ]]; then
       popd &>/dev/null
    done
 elif [[ `is-pwd-a.sh cr:dataset` == "yes" ]]; then
-   for version in `cr-list-versions.sh | tail -1`; do
+   for version in `cr-list-versions.sh $latest_version_only`; do
       pushd version/$version &>/dev/null
          $0 $orig_params # Run this same script with the same params we were given.
       popd &>/dev/null
