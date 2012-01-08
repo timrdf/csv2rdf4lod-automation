654a98d7a69b782d182ec558b9e3b53f74c3f504
Timothy Lebo <timrdf@gmail.com>
2012-01-08 00:01:40 -0500

    initial PATH and CLASSPATH checks/fill in

--- bin/util/cr-situate.sh
+++ bin/util/cr-situate.sh
@@ -2,6 +2,22 @@
 
 # export PATH=$PATH:$CSV2RDF4LOD_HOME/bin:$CSV2RDF4LOD_HOME/bin/util
 
-for path in `echo ${PATH//://  }`; do
-   echo $path
-done
+if [ ! `which cr-vars.sh` ]; then
+   export PATH=$PATH:$CSV2RDF4LOD_HOME/bin
+fi
+
+if [ ! `which prefixes2flags.sh` ]; then
+   export PATH=$PATH:$CSV2RDF4LOD_HOME/bin/dup
+fi
+
+if [ ! `which pcurl.sh` ]; then export PATH=$PATH:$CSV2RDF4LOD_HOME/bin/util
+   export PATH=$PATH:$CSV2RDF4LOD_HOME/bin/util
+fi
+
+if [ ! `which vload` ]; then
+   export PATH=$PATH:$CSV2RDF4LOD_HOME/bin//util/virtuoso
+fi
+
+#for path in `echo ${PATH//://  }`; do
+#   echo $path
+#done
