a9d40e7fd90ff127fb708ddf3e78da337789bcd6
Timothy Lebo <timrdf@gmail.com>
2012-02-08 18:38:44 -0500

    prep load meta into vload graph

--- bin/convert-aggregate.sh
+++ bin/convert-aggregate.sh
@@ -781,10 +781,10 @@ echo "fi"                                                                       
 echo "if [ -e '$lnwwwrootSH' ]; then # Make sure that the file we will load from the web is published"       >> $vloadSH
 echo "  $lnwwwrootSH"                                                                                        >> $vloadSH
 echo "fi"                                                                                                    >> $vloadSH
-echo ""                                                                                                         >> $vloadSH
-echo ""                                                                                                         >> $vloadSH
+echo ""                                                                                                      >> $vloadSH
+echo ""                                                                                                      >> $vloadSH
 echo "graph=\`cat '$SDV.sd_name'\`"                                                                          >> $vloadSH
-echo "metaGraph=\`cat '$SDV.sd_name'\`"                                                                      >> $vloadSH
+echo "metaGraph=\\$graph\`"                                                                                  >> $vloadSH
 echo "if [ \"\$1\" == \"--sample\" ]; then"                                                                  >> $vloadSH
 http_allRawSample="\${CSV2RDF4LOD_BASE_URI_OVERRIDE:-\$CSV2RDF4LOD_BASE_URI}/source/${sourceID}/file/${datasetID}/version/${versionID}/conversion/${sourceID}-${datasetID}-${datasetVersion}.rdf"
 for layerSlug in $layerSlugs # <---- Add root-level subsets here.
