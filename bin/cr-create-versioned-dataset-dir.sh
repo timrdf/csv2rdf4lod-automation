b319904cf031b001959a60dd0fcc5db774deeb21
Timothy Lebo <timrdf@gmail.com>
2012-02-14 12:04:22 -0500

    license

--- bin/cr-create-versioned-dataset-dir.sh
+++ bin/cr-create-versioned-dataset-dir.sh
@@ -2,6 +2,20 @@
 #
 # https://github.com/timrdf/csv2rdf4lod-automation/blob/master/bin/cr-create-versioned-dataset-dir.sh
 #
+#   Copyright 2012 Timothy Lebo
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
 # This script sets up a new version of a dataset when given a URL to a tabular file and some options
 # describing its structure (comment character, header line, and delimter).
 #
--- bin/setup.sh
+++ bin/setup.sh
@@ -1,3 +1,18 @@
+#
+#   Copyright 2012 Timothy Lebo
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
 # _NOTE_    setup.sh (this file) is a template used by install.sh to create source-me.sh.
 # _WARNING_ setup.sh (this file) should not be edited. Edit my-csv2rdf4lod-source-me.sh.
 # if path of cr-vars.sh 
