8c9e33f977ff48f670c863c84ef6457aba888ad2
Timothy Lebo <timrdf@gmail.com>
2012-02-14 10:13:23 -0500

    license

--- bin/convert-aggregate.sh
+++ bin/convert-aggregate.sh
@@ -1,18 +1,4 @@
-# convert-aggregate.sh
-#
-#   Copyright 2012 Timothy Lebo
-#
-#   Licensed under the Apache License, Version 2.0 (the "License");
-#   you may not use this file except in compliance with the License.
-#   You may obtain a copy of the License at
-#
-#       http://www.apache.org/licenses/LICENSE-2.0
-#
-#   Unless required by applicable law or agreed to in writing, software
-#   distributed under the License is distributed on an "AS IS" BASIS,
-#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#   See the License for the specific language governing permissions and
-#   limitations under the License.
+# <> prov:specializationOf <https://raw.github.com/timrdf/csv2rdf4lod-automation/master/bin/convert-aggregate.sh> .
 #
 # Aggregate conversion outputs into publishable datafiles.
 #
@@ -23,6 +9,7 @@
 #
 # ___NOT___ to be called directly: called by convert-DATASET.sh (a script created using cr-create-convert-sh.sh)
 # can also be invoked running publish/bin/publish.sh
+
 if [ ${CSV2RDF4LOD_FORCE_PUBLISH:-"."} == "true" ]; then
    echo "convert-aggregate.sh publishing raw and enhancements (forced)." | tee -a $CSV2RDF4LOD_LOG
    echo "===========================================================================================" | tee -a $CSV2RDF4LOD_LOG
