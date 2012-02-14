7d80333c753a9b9622b050220fb504d3947ea96a
Timothy Lebo <timrdf@gmail.com>
2012-02-14 12:09:52 -0500

    license

--- bin/util/grep-head.sh
+++ bin/util/grep-head.sh
@@ -2,6 +2,20 @@
 #
 # grep-head.awk
 #
+#   Copyright 2012 Timothy Lebo
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
 # Print the end of a file, starting from the line that matches a given pattern.
 # If no pattern is provided, defaults to /^# CSV2RDF4LOD DATASET METADATA.*$/ 
 # (what edu.rpi.tw.data.csv.CSVtoRDF.java outputs to indicate VoID subset).
