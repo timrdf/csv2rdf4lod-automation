fc8176024fccee0d3375a6861cb8695f0be96e27
Timothy Lebo <timrdf@gmail.com>
2012-02-14 10:07:39 -0500

    license

--- bin/convert-aggregate.sh
+++ bin/convert-aggregate.sh
@@ -1,5 +1,19 @@
 # convert-aggregate.sh
 #
+#   Copyright 2012 Timothy Lebo
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
 # Aggregate conversion outputs into publishable datafiles.
 #
 # Up to this point, all processing was done using filenames provided by the dataset source organization.
