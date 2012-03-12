e618b18764f5dcfe228466ec92889f39a5da1267
Timothy Lebo <timrdf@gmail.com>
2012-03-11 21:57:10 -0400

    eparams and retrieval

---
+++ doc/examples/source/lebot/golfers/version/retrieve.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+#3> @prefix doap:    <http://usefulinc.com/ns/doap#> .
+#3> @prefix dcterms: <http://purl.org/dc/terms/> .
+#3> @prefix rdfs:    <http://www.w3.org/2000/01/rdf-schema#> .
+#3> 
+#3> <#>
+#3>    a doap:Project; 
+#3>    dcterms:description 
+#3>      "Script to retrieve and convert a new version of the dataset.";
+#3>    rdfs:seeAlso 
+#3>      <https://github.com/timrdf/csv2rdf4lod-automation/wiki/Automated-creation-of-a-new-Versioned-Dataset>;
+#3> .
+
+export CSV2RDF4LOD_CONVERT_OMIT_RAW_LAYER="true"
+$CSV2RDF4LOD_HOME/bin/cr-create-versioned-dataset-dir.sh cr:auto \
+ 'https://github.com/timrdf/csv2rdf4lod-automation/raw/master/doc/examples/source/lebot/golfers/version/original/manual/golfers.xls.zip'
