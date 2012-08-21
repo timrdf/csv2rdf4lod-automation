78c9d10626f9d5b3ebb1fa865f4fd985f6a9e28d
Timothy Lebo <timrdf@gmail.com>
2012-08-21 19:38:32 -0400

    added some command line guidance, but commented out since it will cluster conversion trigger output

--- bin/util/cr-situate-classpaths.sh
+++ bin/util/cr-situate-classpaths.sh
@@ -41,3 +41,15 @@ for jar in                                                             \
 done
 
 echo $missing
+
+#echo >&2
+#if [ ${#missing} -eq 0 ]; then
+#   echo "Good job. Now all classpaths that csv2rdf4lod-automation needs are on CLASSPATH." >&2
+#   echo ${#missing}
+#else
+#   echo "^^ These classpaths are required by csv2rdf4lod-automation, but are NOT in CLASSPATH." >&2
+#   echo >&2
+#   echo "Put them there by executing:" >&2
+#   echo >&2
+#   echo "    export CLASSPATH=\$CLASSPATH:\`\$CSV2RDF4LOD_HOME/bin/util/cr-situate-classpaths.sh\`" >&2
+#fi
