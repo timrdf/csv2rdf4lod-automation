d36ba795a8afde326a74e72c43c8cbd231790a73
Timothy Lebo <timrdf@gmail.com>
2012-08-26 13:04:47 -0400

    comments

--- bin/util/rdf2nt.sh
+++ bin/util/rdf2nt.sh
@@ -2,6 +2,14 @@
 
 # Accept an RDF file in RDF/XML, Turtle, or N-TRIPLES and output N-TRIPLES or N-QUADS file.
 # N-TRIPLES output will be produce collision-safe bnodes (b/c will prepend with filepath hash).
+#
+# example usages:
+#
+# Be careful not to process the output as input:
+#   rdf2nt.sh *.* > ../all.nt
+#
+# To handle more files than 'ls' can provide:
+#   find . -name "[^.]*" | xargs      rdf2nt.sh > ../all.nt
 
 while [ $# -gt 0 ]; do
    file="$1" 
@@ -15,11 +23,15 @@ while [ $# -gt 0 ]; do
       serialization="-g" # Assume that the extension is correct
    fi
    fullpath=`pwd`/$1 # Does not need to be exact; only needs to be unique.
-   md5=`md5 -qs $fullpath`
+   md5=urlhash`md5 -qs $fullpath`
    
+   # -q : quiet
+   # -o : output ntriples
    # rapper cannot contextualize bnodes and may lead to a collision.
-   # serdi can, but cannot handle RDF/XML (so use rapper to preprocess it)
    rapper -q $serialization -o ntriples $file | serdi -i ntriples -p $md5 -
-
+   # serdi can, but cannot handle RDF/XML (so use rapper to preprocess it).
+   # -p : prepend bnodes with $md5
+   # -  : read from stdin
+   # (prints to stdout)
    shift
 done
