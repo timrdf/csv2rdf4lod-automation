c9fd17993125d29d949ed52f166bebb419b06e4d
Timothy Lebo <timrdf@gmail.com>
2012-08-30 12:18:36 -0400

    wrapper shell for java resource name

--- bin/util/resource-name.sh
+++ bin/util/resource-name.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-CLASSPATH=$CLASSPATH`$CSV2RDF4LOD_HOME/bin/util/cr-situate-classpaths.sh`
-
-java edu.rpi.tw.string.NameFactory $*
+java -cp $CLASSPATH`$CSV2RDF4LOD_HOME/bin/util/cr-situate-classpaths.sh` edu.rpi.tw.string.NameFactory $*
