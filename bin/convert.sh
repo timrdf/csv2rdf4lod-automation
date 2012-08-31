6274968ebec52c083e6724db8e2116db7b726c1b
Timothy Lebo <timrdf@gmail.com>
2012-08-31 10:55:09 -0400

    fixed https://github.com/timrdf/csv2rdf4lod-automation/issues/288

--- bin/convert.sh
+++ bin/convert.sh
@@ -43,22 +43,20 @@ export CLASSPATH=$CLASSPATH`$CSV2RDF4LOD_HOME/bin/util/cr-situate-classpaths.sh`
 
 # Java dependencies; relative to $CSV2RDF4LOD_HOME
 # DEPRECATED. Changes should migrate to bin/util/cr-situate-classpaths.sh
-for jar in                                                             \
-           lib/javacsv2.0/javacsv.jar                                  \
-           bin/dup/openrdf-sesame-2.3.1-onejar.jar                     \
-           bin/dup/slf4j-api-1.5.6.jar                                 \
-           bin/dup/slf4j-nop-1.5.6.jar                                 \
-           lib/joda-time-2.0/joda-time-2.0.jar                         \
-           bin/dup/datadigest-1.0-SNAPSHOT.jar                         \
-           bin/lib/commons-validator-1.3.1/commons-validator-1.3.1.jar \
-           bin/dup/csv2rdf4lod.jar ; do
-   if [[ $CLASSPATH != *`basename $jar`* ]]; then
-      if [ ${CSV2RDF4LOD_CONVERT_DEBUG_LEVEL:="."} == "fine" ]; then
-         echo "`basename $jar` not in classpath; adding $CSV2RDF4LOD_HOME/$jar"
-      fi
-      export CLASSPATH=$CLASSPATH:$CSV2RDF4LOD_HOME/$jar # TODO: export? : vs ; cygwin
-   fi
-done
+#for jar in                                                             \
+#           lib/javacsv2.0/javacsv.jar                                  \
+#           bin/dup/openrdf-sesame-2.3.1-onejar.jar                     \
+#           bin/dup/slf4j-api-1.5.6.jar                                 \
+#           bin/dup/slf4j-nop-1.5.6.jar                                 \
+#           lib/joda-time-2.0/joda-time-2.0.jar                         \
+#           bin/dup/datadigest-1.0-SNAPSHOT.jar                         \
+#           bin/lib/commons-validator-1.3.1/commons-validator-1.3.1.jar \
+#           bin/dup/csv2rdf4lod.jar ; do
+#   if [[ $CLASSPATH != *`basename $jar`* ]]; then
+#      echo "`basename $jar` not in classpath; adding $CSV2RDF4LOD_HOME/$jar"
+#      export CLASSPATH=$CLASSPATH:$CSV2RDF4LOD_HOME/$jar # TODO: export? : vs ; cygwin
+#   fi
+#done
 
 eParamsDir=manual # Enhancement parameter templates are placed in manual/ b/c a human will be modifying them.
 
