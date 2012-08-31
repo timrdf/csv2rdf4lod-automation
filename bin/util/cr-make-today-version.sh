adf7cb2ff0762fa3fb2e906420f8146c4e6a5921
Timothy Lebo <timrdf@gmail.com>
2012-08-31 11:24:48 -0400

    make today version recursive

--- bin/util/cr-make-today-version.sh
+++ bin/util/cr-make-today-version.sh
@@ -22,20 +22,31 @@ fi
 CSV2RDF4LOD_HOME=${CSV2RDF4LOD_HOME:?"not set; source csv2rdf4lod/source-me.sh or see https://github.com/timrdf/csv2rdf4lod-automation/wiki/CSV2RDF4LOD-not-set"}
 
 # cr:data-root cr:source cr:directory-of-datasets cr:dataset cr:directory-of-versions cr:conversion-cockpit
-ACCEPTABLE_PWDs="cr:directory-of-versions"
+ACCEPTABLE_PWDs="cr:dataset cr:directory-of-versions"
 if [ `${CSV2RDF4LOD_HOME}/bin/util/is-pwd-a.sh $ACCEPTABLE_PWDs` != "yes" ]; then
    ${CSV2RDF4LOD_HOME}/bin/util/pwd-not-a.sh $ACCEPTABLE_PWDs
    exit 1
 fi
 
-# TODO: use date +%Y-%b-%d
-dir=`dateInXSDDateTime.sh | sed -e 's/T.*$//' -e 's/-/ /g' | awk '{abbr["01"]="Jan";abbr["02"]="Feb";abbr["03"]="Mar";abbr["04"]="Apr";abbr["05"]="May";abbr["06"]="Jun";abbr["07"]="Jul";abbr["08"]="Aug";abbr["09"]="Sep";abbr["10"]="Oct";abbr["11"]="Nov";abbr["12"]="Dec"; printf("%s-%s-%s",$1,abbr[$2],$3)}'`
-if [ ! -e $dir -a ${1:-"."} == '-w' ]; then
-   mkdir $dir
-   echo $dir
-   mkdir $dir/source
-   echo $dir/source
-else
-   echo $dir
-   echo "use -w to make dir"
+if [[ `is-pwd-a.sh                                                            cr:directory-of-versions` == "yes" ]]; then
+
+   #dir=`dateInXSDDateTime.sh | sed -e 's/T.*$//' -e 's/-/ /g' | awk '{abbr["01"]="Jan";abbr["02"]="Feb";abbr["03"]="Mar";abbr["04"]="Apr";abbr["05"]="May";abbr["06"]="Jun";abbr["07"]="Jul";abbr["08"]="Aug";abbr["09"]="Sep";abbr["10"]="Oct";abbr["11"]="Nov";abbr["12"]="Dec"; printf("%s-%s-%s",$1,abbr[$2],$3)}'`
+
+   dir=`date +%Y-%b-%d`
+   if [ ! -e $dir -a ${1:-"."} == '-w' ]; then
+      mkdir $dir
+      echo $dir
+      mkdir $dir/source
+      echo $dir/source
+   else
+      echo $dir
+      echo "use -w to make dir"
+   fi
+elif [[ `is-pwd-a.sh                                                cr:dataset                         ` == "yes" ]]; then
+   if [ ! -e version ]; then
+      mkdir version # See https://github.com/timrdf/csv2rdf4lod-automation/wiki/Directory-Conventions
+   fi
+   pushd version > /dev/null
+      $0 $* # Recursive call
+   popd > /dev/null 
 fi
