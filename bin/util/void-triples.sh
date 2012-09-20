cb1c38383787a72436b7a14f2bb761297dc1c0da
Timothy Lebo <timrdf@gmail.com>
2012-09-20 14:23:10 -0400

    void count

--- bin/util/void-triples.sh
+++ bin/util/void-triples.sh
@@ -6,14 +6,35 @@
 # rapper: Parsing returned 17 triples
 
 count=0
-while [ $# -gt 0 ]; do
-   file="$1"
-   if [ -e $file ]; then
-      c=`rapper -g -c $file 2>&1 | grep "Parsing returned [^ ]* triples" | awk '{printf($4)}'`
-      if [ ${#c} -gt 0 ]; then
-         let "count=count+c"
+if [ $# -gt 0 ]; then
+
+   while [ $# -gt 0 ]; do
+      file="$1"
+      if [ -e $file ]; then
+         c=`rapper -g -c $file 2>&1 | grep "Parsing returned [^ ]* triples" | awk '{printf($4)}'`
+         if [ ${#c} -gt 0 ]; then
+            let "count=count+c"
+         fi
       fi
+      shift
+   done
+
+else
+   if   [[ `is-pwd-a.sh cr:conversion-cockpit` == "yes" ]]; then
+
+      grep "void:triples" automatic/*.void.ttl | sed 's/[^0-9]*//g' | awk '{s+=$1} END {print s}'
+
+   else
+
+      for possible in `find . -name "convert*.sh"`; do
+         pushd `dirname $possible` > /dev/null
+            c=`$0 $*` # Recursive call
+            if [ ${#c} -gt 0 ]; then
+               let "count=count+c"
+            fi
+         popd > /dev/null
+      done
+
    fi
-   shift
-done
+fi
 echo $count
