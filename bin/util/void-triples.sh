7a5385963740b7151bb000c04b6e94924ee3d95d
Timothy Lebo <timrdf@gmail.com>
2012-09-20 14:24:19 -0400

    void count

--- bin/util/void-triples.sh
+++ bin/util/void-triples.sh
@@ -30,6 +30,7 @@ else
          pushd `dirname $possible` > /dev/null
             c=`$0 $*` # Recursive call
             if [ ${#c} -gt 0 ]; then
+               echo adding $c
                let "count=count+c"
             fi
          popd > /dev/null
