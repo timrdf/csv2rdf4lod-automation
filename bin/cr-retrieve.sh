1857933956519f3c0f98f8f4b36dff51f0601d9e
Timothy Lebo <timrdf@gmail.com>
2012-09-21 14:02:16 -0400

    less output

--- bin/cr-retrieve.sh
+++ bin/cr-retrieve.sh
@@ -41,7 +41,7 @@ elif [[ `is-pwd-a.sh                                                            
    if [ -e "$dcat" ]; then
       url=`grep "dcat:downloadURL" $dcat | head -1 | awk '{print $2}' | sed 's/<//; s/>.*$//'` # TODO: query it as RDF...
       if [ "$dryrun" != "yes" ]; then
-         echo template from $0 pwd: `pwd`
+         #echo template from $0 pwd: `pwd`
          cat $0.template > retrieve.sh # NOTE: chmod +w /opt/csv2rdf4lod-automation/bin/cr-retrieve.sh.template
          perl -pi -e "s|DOWNLOAD_URL|$url|" retrieve.sh
          chmod +x retrieve.sh
