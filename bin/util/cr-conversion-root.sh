6377cfc9e6c4ebf41733ad386e355ff235fc3bad
Timothy Lebo <timrdf@gmail.com>
2012-10-12 17:48:52 -0400

    --quiet

--- bin/util/cr-conversion-root.sh
+++ bin/util/cr-conversion-root.sh
@@ -11,14 +11,18 @@ if [[ "$1" == "--help" || "$1" == "-h" ]]; then
    exit 1
 fi
 
-CSV2RDF4LOD_HOME=${CSV2RDF4LOD_HOME:?"not set; source csv2rdf4lod/source-me.sh or see https://github.com/timrdf/csv2rdf4lod-automation/wiki/CSV2RDF4LOD-not-set"}
+see='https://github.com/timrdf/csv2rdf4lod-automation/wiki/CSV2RDF4LOD-not-set'
+CSV2RDF4LOD_HOME=${CSV2RDF4LOD_HOME:?"not set; source csv2rdf4lod/source-me.sh or see $see"}
 
 # cr:data-root cr:source cr:directory-of-datasets cr:dataset cr:directory-of-versions cr:conversion-cockpit
 ACCEPTABLE_PWDs="cr:data-root cr:source cr:directory-of-datasets cr:dataset cr:directory-of-versions cr:conversion-cockpit"
 if [ `${CSV2RDF4LOD_HOME}/bin/util/is-pwd-a.sh $ACCEPTABLE_PWDs` != "yes" ]; then
-   ${CSV2RDF4LOD_HOME}/bin/util/pwd-not-a.sh $ACCEPTABLE_PWDs
+   if [ "$1" != "--quiet" ]; then
+      ${CSV2RDF4LOD_HOME}/bin/util/pwd-not-a.sh $ACCEPTABLE_PWDs
+   fi
    exit 1
 fi
+
 if   [[ `is-pwd-a.sh cr:conversion-cockpit` == "yes" ]]; then
 
    # e.g. 
