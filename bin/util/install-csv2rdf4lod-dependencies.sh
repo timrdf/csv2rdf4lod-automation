73981c40fa166a46cf7bae4d80464f3e15cb270a
Timothy Lebo <timrdf@gmail.com>
2012-10-15 18:36:05 -0400

    install virt

--- bin/util/install-csv2rdf4lod-dependencies.sh
+++ bin/util/install-csv2rdf4lod-dependencies.sh
@@ -115,9 +115,12 @@ if [ "$install_it" == "y" ]; then
    url='http://sourceforge.net/projects/virtuoso/files/latest/download'
    pushd $base &> /dev/null
       sudo touch pid.$$
-      redirect=`curl -sLI $url | grep "^Location:" | tail -1 | sed 's/[^z]*$/\n/g' | awk '{printf("%s\n",$2)}'`
+      #redirect=`curl -sLI $url | grep "^Location:" | tail -1 | sed 's/[^z]*$/\n/g' | awk '{printf("%s\n",$2)}'`
       # ^ e.g. http://superb-dca3.dl.sourceforge.net/project/virtuoso/virtuoso/6.1.6/virtuoso-opensource-6.1.6.tar.gz
-      tarball=`basename $redirect`
+      redirect=$url
+
+      #tarball=`basename $redirect`
+      tarball=virtuoso.tar.gz
       # ^ e.g. virtuoso-opensource-6.1.6.tar.gz
       echo "${redirect}.----------"
       echo to
