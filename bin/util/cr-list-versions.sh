f17d3e845079625be79a533c5bb6d5c11bf360d2
Timothy Lebo <timrdf@gmail.com>
2012-12-05 08:16:43 -0500

    tweaks

--- bin/util/cr-list-versions.sh
+++ bin/util/cr-list-versions.sh
@@ -22,19 +22,20 @@
 #   $ cr-list-versions.sh 
 #     2011-Apr-20
 #     2011-Jun-15
-#     2011-Aug-10        # Sorted by last modified date
+#     2011-Aug-10        # Sorted by last modified date (newest last)
 #   $ touch 2011-Jun-15
 #   $ cr-list-versions.sh 
 #     2011-Apr-20
-#     2011-Aug-10        # Sorted by last modified date
-#     2011-Jun-15        
+#     2011-Aug-10
+#     2011-Jun-15        # Sorted by last modified date (newest last) 
 #   $ touch 2011-Aug-10
 #   $ cr-list-versions.sh 
 #     2011-Apr-20
 #     2011-Jun-15
-#     2011-Aug-10        # Sorted by last modified date
+#     2011-Aug-10        # Sorted by last modified date (newest last)
 
-CSV2RDF4LOD_HOME=${CSV2RDF4LOD_HOME:?"not set; source csv2rdf4lod/source-me.sh or see https://github.com/timrdf/csv2rdf4lod-automation/wiki/CSV2RDF4LOD-not-set"}
+see='https://github.com/timrdf/csv2rdf4lod-automation/wiki/CSV2RDF4LOD-not-set'
+CSV2RDF4LOD_HOME=${CSV2RDF4LOD_HOME:?"not set; source csv2rdf4lod/source-me.sh or see $see"}
 
 # cr:data-root cr:source cr:directory-of-datasets cr:dataset cr:directory-of-versions cr:conversion-cockpit
 ACCEPTABLE_PWDs="cr:dataset cr:directory-of-versions cr:conversion-cockpit"
@@ -45,12 +46,14 @@ fi
 
 TEMP="_"`basename $0``date +%s`_$$.tmp
 
-if   [[ `${CSV2RDF4LOD_HOME}/bin/util/is-pwd-a.sh cr:dataset`                                                == "yes" ]]; then
+if   [[ `${CSV2RDF4LOD_HOME}/bin/util/is-pwd-a.sh cr:source`                                                 == "yes" ]]; then
+   echo TODO
+elif [[ `${CSV2RDF4LOD_HOME}/bin/util/is-pwd-a.sh           cr:dataset`                                      == "yes" ]]; then
    #find version -mindepth 1 -maxdepth 1 -type d | grep -v "/\." | sed 's/^version.//'
    pushd version &> /dev/null
       $0 $* # Call this script again
    popd &> /dev/null
-elif [[ `${CSV2RDF4LOD_HOME}/bin/util/is-pwd-a.sh            cr:directory-of-versions`                       == "yes" ]]; then
+elif [[ `${CSV2RDF4LOD_HOME}/bin/util/is-pwd-a.sh                     cr:directory-of-versions`              == "yes" ]]; then
    #find .       -mindepth 1 -maxdepth 1 -type d | grep -v "/\." | sed 's/^\.\///'
    # List directories and when they were modified, bring to one line, sort so oldest is first, get rid of hidden directories.
    find . -mindepth 1 -maxdepth 1                               \
