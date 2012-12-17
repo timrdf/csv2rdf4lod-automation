7d9a25ed277e79b5cdf46fd18d58284038b14579
Timothy Lebo <timrdf@gmail.com>
2012-12-17 12:42:13 -0500

    hashing distributions

--- bin/cr-create-dataset-dirs-from-ckan.py
+++ bin/cr-create-dataset-dirs-from-ckan.py
@@ -88,7 +88,7 @@ for name in ckan.package_register_get():
          'SOURCE_ID'            : sourceID,
          'DATASET_ID'           : dataset['name'],
          'FORMAT'               : fmt.replace('"','\''),
-         'FORMAT_R'             : fmt.replace(' ','_').lower(),
+         'FORMaT'               : fmt.replace(' ','_').lower(),
          'UUID'                 : str(uuid.uuid4()),
          'SOURCE_CKAN'          : ckanAPI.replace('/api',''),
          'SOURCE_AGENT'         : re.sub('(http://[^/]*)/.*$','\\1',ckanAPI),
@@ -117,18 +117,18 @@ for name in ckan.package_register_get():
    a void:Dataset, dcat:Dataset;
    conversion:source_identifier  "SOURCE_ID";
    conversion:dataset_identifier "DATASET_ID";
-   prov:wasDerivedFrom :as_a_FORMAT_R_UUID;
+   prov:wasDerivedFrom :as_a_FORMAT_UUID;
 .
 
-:as_a_FORMAT_R_UUID
+:as_a_FORMAT_UUID
    a dcat:Distribution;
    dcat:downloadURL <DIST_URL>;
-   dcterms:format [ rdfs:label "FORMAT" ];
+   dcterms:format [ rdfs:label "FORMaT" ];
 .
 
 <SOURCE_CKAN/dataset/DATASET_ID>
    a dcat:Dataset, datafaqs:CKANDataset;
-   dcat:distribution :as_a_FORMAT_R_UUID;
+   dcat:distribution :as_a_FORMAT_UUID;
    prov:wasAttributedTo <SOURCE_AGENT>;
 .
 '''
