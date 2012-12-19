bcfc77aecc60ef839508c8bd48123d34225a3bee
Timothy Lebo <timrdf@gmail.com>
2012-12-18 22:51:27 -0500

    cleaning output

--- bin/cr-publish-droid-to-endpoint.sh
+++ bin/cr-publish-droid-to-endpoint.sh
@@ -106,7 +106,7 @@ for droid in `find . -mindepth 6 -maxdepth 6 -name cr-droid.ttl`; do
 
    #ext=${droid%*.}
    #let "tally=tally+1
-   echo "ln $droid -> $sdv.ttl"
+   echo "ln $sdv.ttl"
    if [ "$dryRun" != "true" ]; then
       ln $droid $cockpit/source/$sdv.ttl
    fi
