10aaf7736e37d7cf5a2516910ea33541687bd559
Timothy Lebo <timrdf@gmail.com>
2012-12-19 11:21:43 -0500

    turtle

--- bin/util/dateInXSDDateTime.sh
+++ bin/util/dateInXSDDateTime.sh
@@ -21,6 +21,8 @@ if [ $# -eq 0 ]; then
    date +%Y-%m-%dT%H:%M:%S%z | sed 's/^\(.*\)\(..\)$/\1:\2/'
 elif [ $1 == "coin:slug" ]; then
    date +%Y-%m-%dT%H_%M_%S%z | sed 's/^\(.*\)\(..\)$/\1:\2/' | sed 's/:/-/g; s/\+/-/g'
+elif [ $1 == "--turtle" ]; then
+   date +%Y-%m-%dT%H:%M:%S%z | sed 's/^\(.*\)\(..\)$/\1:\2/' | awk '{print"\""$0"\"^^xsd:dateTime"}'
 else
    date +%Y-%m-%dT%H:%M:%S%z | sed 's/^\(.*\)\(..\)$/\1:\2/'
 fi
