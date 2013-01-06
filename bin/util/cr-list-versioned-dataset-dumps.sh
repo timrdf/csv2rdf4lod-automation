c57431c022e177f12fe296ca7020da3b9c739e66
Timothy Lebo <timrdf@gmail.com>
2013-01-06 11:37:49 -0500

    listed version dumps

---
+++ bin/util/cr-list-versioned-dataset-dumps.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+#
+#3> <> prov:specializationOf <https://github.com/timrdf/csv2rdf4lod-automation/blob/master/bin/util/cr-list-versioned-dataset-dumps.sh>;
+#3>    prov:wasDerivedFrom <https://github.com/timrdf/csv2rdf4lod-automation/blob/master/bin/cr-publish-cockpit.sh> .
+#
+
+if [[ "$1" == "--help" || "$1" == "-h" ]]; then
+   echo "usage: `basename $0` [-w]"
+   echo "  Create publish/bin/publish.sh and invoke for every conversion cockpit within the current directory tree."
+   echo "  -w : Avoid dryrun; do it. If not provided, will only dry run."
+fi
+
+see='https://github.com/timrdf/csv2rdf4lod-automation/wiki/CSV2RDF4LOD-not-set'
+CSV2RDF4LOD_HOME=${CSV2RDF4LOD_HOME:?"not set; source csv2rdf4lod/source-me.sh or see $see"}
+
+# cr:data-root cr:source cr:directory-of-datasets cr:dataset cr:directory-of-versions cr:conversion-cockpit
+ACCEPTABLE_PWDs="cr:data-root cr:source cr:directory-of-datasets cr:dataset cr:directory-of-versions cr:conversion-cockpit"
+if [ `${CSV2RDF4LOD_HOME}/bin/util/is-pwd-a.sh $ACCEPTABLE_PWDs` != "yes" ]; then
+   echo `pwd` `cr-pwd.sh` `cr-pwd-type.sh` `${CSV2RDF4LOD_HOME}/bin/util/is-pwd-a.sh $ACCEPTABLE_PWDs`
+   ${CSV2RDF4LOD_HOME}/bin/util/pwd-not-a.sh $ACCEPTABLE_PWDs
+   exit 1
+fi
+
+write="no"
+if [[ "$1" == "-w" || "$1" == "--write" ]]; then
+   write="yes"
+fi
+
+TEMP="_"`basename $0``date +%s`_$$.tmp
+
+if [[ `is-pwd-a.sh cr:conversion-cockpit` == "yes" ]]; then
+
+   echo "========== `cr-pwd.sh` ========================================"
+   echo
+
+   if [ "$write" == "yes" ]; then
+      export CSV2RDF4LOD_FORCE_PUBLISH="true"
+      source $CSV2RDF4LOD_HOME/bin/convert-aggregate.sh
+      export CSV2RDF4LOD_FORCE_PUBLISH="false"
+   fi
+
+   if [ "$write" == "no" ]; then
+      echo
+      echo "Note: Performed dry run only; no changes occurred. Use `basename $0` -w to avoid dry run and make modifications."
+      echo
+   fi
+
+elif [[ `is-pwd-a.sh cr:data-root cr:source cr:directory-of-datasets            cr:directory-of-versions` == "yes" ]]; then
+   for next in `directories.sh`; do
+      pushd $next > /dev/null
+         # Recursive call to base case 'cr:conversion-cockpit'
+         $0 $*
+      popd > /dev/null
+   done
+elif [[ `is-pwd-a.sh              cr:source                                                             ` == "yes" ]]; then
+   # TODO
+   pushd dataset > /dev/null
+      # Recursive call to base case 'cr:conversion-cockpit'
+      $0 $*
+   popd > /dev/null
+elif [[ `is-pwd-a.sh                                                 cr:dataset                         ` == "yes" ]]; then
+   pushd version > /dev/null
+      # Recursive call to base case 'cr:conversion-cockpit'
+      $0 $*
+   popd > /dev/null
+fi
+
+if [ -e $TEMP ]; then
+   rm -f $TEMP
+fi
