5874892367be827f1cfcaa002acee928182e6aa6
Timothy Lebo <timrdf@gmail.com>
2013-01-06 09:14:02 -0500

    full dump

--- bin/util/cr-full-dump.sh
+++ bin/util/cr-full-dump.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 #
+#3> <> prov:specializationOf <https://github.com/timrdf/csv2rdf4lod-automation/blob/master/bin/util/cr-full-dump.sh>;
+#3>   prov:wasDerivedFrom <cr-publish-droid-to-endpoint.sh> .
+#
 # Gather all versioned dataset dump files into one enormous dump file.
 # This is highly redundant, but can be helpful for those that "just want the data"
 # and don't want to crawl the VoID dataDumps to get it.
@@ -9,6 +12,9 @@ CSV2RDF4LOD_HOME=${CSV2RDF4LOD_HOME:?"not set; source csv2rdf4lod/source-me.sh o
 
 see="https://github.com/timrdf/csv2rdf4lod-automation/wiki/Aggregating-subsets-of-converted-datasets"
 CSV2RDF4LOD_PUBLISH_OUR_SOURCE_ID=${CSV2RDF4LOD_PUBLISH_OUR_SOURCE_ID:?"not set; see $see"}
+
+see="https://github.com/timrdf/csv2rdf4lod-automation/wiki/Aggregating-subsets-of-converted-datasets"
+CSV2RDF4LOD_BASE_URI=${CSV2RDF4LOD_BASE_URI:?"not set; see $see"}
 
 # cr:data-root cr:source cr:directory-of-datasets cr:dataset cr:directory-of-versions cr:conversion-cockpit
 ACCEPTABLE_PWDs="cr:data-root"
@@ -21,18 +27,47 @@ TEMP="_"`basename $0``date +%s`_$$.tmp
 
 sourceID=$CSV2RDF4LOD_PUBLISH_OUR_SOURCE_ID
 datasetID=`basename $0 | sed 's/.sh$//'`
-versionID=`date +%Y-%b-%d`
+versionID='latest' # Doing it every day is a waste of space for this use case. `date +%Y-%b-%d`
+
+cockpit="$sourceID/$datasetID/version/$versionID"
+dumpFileLocal=`echo $CSV2RDF4LOD_BASE_URI | perl -pi -e 's|http://||;s/\./-/g'`
 
 if [[ $# -lt 1 || "$1" == "--help" ]]; then
-   echo "usage: `basename $0` [--target] [-n] --clear-graph <named_graph_URI | cr:auto | .>"
+   echo "usage: `basename $0` [--target] [-n]"
    echo ""
    echo "  Gather all versioned dataset dump files into one enormous dump file."
    echo "    archive them into a versioned dataset 'latest'"
    echo ""
-   echo "         --target : return the name of graph that will be loaded; then quit."
+   echo "         --target : return the dump file location, then quit."
    echo "               -n : perform dry run only; do not load named graph."
    echo
    exit 1
 fi
+
+if [ "$1" == "--target" ]; then
+   # a conversion:VersionedDataset:
+   # e.g. http://purl.org/twc/health/source/tw-rpi-edu/dataset/cr-publish-dcat-to-endpoint/version/2012-Sep-07
+   echo $cockpit/publish/$dumpFileLocal
+   exit 0
+fi
+
+dryRun="false"
+if [ "$1" == "-n" ]; then
+   dryRun="true"
+   dryrun.sh $dryrun beginning
+   shift
+fi
+
+if [ ! -d $cockpit/source ]; then
+   mkdir -p $cockpit/source
+fi
+rm -rf $cockpit/source/*
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+for publishDir in `find . -mindepth 5 -maxdepth 5 -type d -name publish`; do
+   echo $publishDir
+done
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 # hub-healthdata-gov/food-recalls/version/2012-May-08/publish/hub-healthdata-gov-food-recalls-2012-May-08.ttl.gz
