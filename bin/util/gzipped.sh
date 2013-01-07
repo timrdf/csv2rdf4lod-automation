e2be8a960d52780eba43214c615cb86608155029
Timothy Lebo <timrdf@gmail.com>
2013-01-07 13:27:27 -0500

    gzipped

--- bin/util/gzipped.sh
+++ bin/util/gzipped.sh
@@ -6,11 +6,19 @@
 # Return "yes" if any of the files listed gzipped.
 # Otherwise, return "no".
 #
+# Usage:
+#
+#   $ gzipped.sh purl-org-twc-health.nt.gz 
+#   yes
+#
+#   $ gzipped.sh purl-org-twc-health.nt
+#   no
+#
 # This logic is also in:
 #    bin/util/rdf2nt.sh - was done before, can switch to this.
 
 if [ $# -lt 1 ]; then
-   echo "usage: `basename $0` a.ttl b.ttl ..."
+   echo "usage: `basename $0` <file>*"
    exit 1
 fi
 
