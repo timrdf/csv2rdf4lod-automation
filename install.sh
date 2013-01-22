f45b0c23581df039701fa2e75ef436f1d470d05b
Timothy Lebo <timrdf@gmail.com>
2013-01-21 19:37:43 -0500

    install prizms

--- install.sh
+++ install.sh
@@ -15,90 +15,109 @@
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
+#
+# Usage:
+#   install.sh [--csh] [--non-interactive]
+#   --csh : seat up for csh instead of bash.
 
 ext="sh"
 if [ "$1" == "--csh" ]; then
    ext="csh"
+   shift
+elif [ "$1" == "--cygwin" ]; then
+   ext="cyg"
+   shift
+fi
+
+interactive="yes"
+if [ "$1" == "--non-interactive" ]; then
+   interactive="no"
 fi
 
 CSV2RDF4LOD_HOME=`pwd`
-echo "#3 <#> a <http://purl.org/twc/vocab/conversion/CSV2RDF4LOD_environment_variables> ;"    > my-csv2rdf4lod-source-me.${ext}
-echo "#3     rdfs:seeAlso"                                                                   >> my-csv2rdf4lod-source-me.${ext}
-echo "#3     <https://github.com/timrdf/csv2rdf4lod-automation/wiki/CSV2RDF4LOD-environment-variables-%28considerations-for-a-distributed-workflow%29>," >> my-csv2rdf4lod-source-me.${ext}
-echo "#3     <https://github.com/timrdf/csv2rdf4lod-automation/wiki/Script:-source-me.sh>" . >> my-csv2rdf4lod-source-me.${ext}
-echo ""                                                                                     >> my-csv2rdf4lod-source-me.${ext}
-echo "export CSV2RDF4LOD_HOME=\"$CSV2RDF4LOD_HOME\""                                        >> my-csv2rdf4lod-source-me.${ext}
+see='https://github.com/timrdf/csv2rdf4lod-automation/wiki/CSV2RDF4LOD-environment-variables-%28considerations-for-a-distributed-workflow%29'
+echo "#3> <#> a <http://purl.org/twc/vocab/conversion/CSV2RDF4LOD_environment_variables> ;"    > my-csv2rdf4lod-source-me.${ext}
+echo "#3>     rdfs:seeAlso"                                                                   >> my-csv2rdf4lod-source-me.${ext}
+echo "#3>     <$see>,"                                                                        >> my-csv2rdf4lod-source-me.${ext}
+echo "#3>     <https://github.com/timrdf/csv2rdf4lod-automation/wiki/Script:-source-me.sh>" . >> my-csv2rdf4lod-source-me.${ext}
+echo ""                                                                                       >> my-csv2rdf4lod-source-me.${ext}
+echo "export CSV2RDF4LOD_HOME=\"$CSV2RDF4LOD_HOME\""                                          >> my-csv2rdf4lod-source-me.${ext}
 echo "`basename $0`:"
 echo "   has created my-csv2rdf4lod-source-me.${ext}."
 echo "   has set \$CSV2RDF4LOD_HOME to $CSV2RDF4LOD_HOME in my-csv2rdf4lod-source-me.${ext}"
 
-cat bin/setup.sh | grep -v "# _NOTE_" | grep -v "# _WARNING_" >> my-csv2rdf4lod-source-me.sh
+cat bin/setup.sh | grep -v "# _NOTE_" | grep -v "# _WARNING_"                                 >> my-csv2rdf4lod-source-me.sh
 
-if [ "$1" == "--csh" ]; then
+if [ "$ext" == "csh" ]; then
  # perl -pi -e
    perl -pe 's/export [^ =]*$//; s/^(\w+)="([^"]*)"/setenv \1 "\2"/; s/export (\S+)="([^"]*)"/setenv \1 "\2"/' \
          my-csv2rdf4lod-source-me.sh > my-csv2rdf4lod-source-me.csh
-elif [ "$1" == "--cygwin" ]; then
+elif [ "$ext" == "cyg" ]; then
    echo "--cygwin not implemented"
 fi
 
 echo ""
 
 mv install.sh bin/
-source my-csv2rdf4lod-source-me.${ext} &> /dev/null
 
-echo
-echo "~~~ What to do next: ~~~"
-echo
-echo "   'source my-csv2rdf4lod-source-me.${ext}' to set the environment variables that cvs2rdf4lod-automation needs to run."
-echo "    sourcing my-csv2rdf4lod-source-me.${ext} must be done each time you log in, so consider adding this to your .bashrc:"
-echo ""
-echo "      source $CSV2RDF4LOD_HOME/my-csv2rdf4lod-source-me.${ext} # http://purl.org/twc/id/software/csv2rdf4lod"
-echo ""
+if [ "$interactive" == "no" ]; then
+   cat my-csv2rdf4lod-source-me.${ext}
+else
+   source my-csv2rdf4lod-source-me.${ext} &> /dev/null
 
-echo
-echo "Put the following 'source' command into your ~/.bashrc?"
-echo "      source $CSV2RDF4LOD_HOME/my-csv2rdf4lod-source-me.${ext} # http://purl.org/twc/id/software/csv2rdf4lod"
-echo -n "(y/N) "
-read -u 1 install_it
-echo
-if [ "$install_it" == "y" ]; then
-   echo                                                                                                          >> ~/.bashrc
-   echo "source $CSV2RDF4LOD_HOME/my-csv2rdf4lod-source-me.${ext} # http://purl.org/twc/id/software/csv2rdf4lod" >> ~/.bashrc
+   echo
+   echo "~~~ What to do next: ~~~"
+   echo
+   echo "   'source my-csv2rdf4lod-source-me.${ext}' to set the environment variables that cvs2rdf4lod-automation needs to run."
+   echo "    sourcing my-csv2rdf4lod-source-me.${ext} must be done each time you log in, so consider adding this to your .bashrc:"
+   echo ""
+   echo "      source $CSV2RDF4LOD_HOME/my-csv2rdf4lod-source-me.${ext} # http://purl.org/twc/id/software/csv2rdf4lod"
+   echo ""
+
+   echo
+   echo "Put the following 'source' command into your ~/.bashrc?"
+   echo "      source $CSV2RDF4LOD_HOME/my-csv2rdf4lod-source-me.${ext} # http://purl.org/twc/id/software/csv2rdf4lod"
+   echo -n "(y/N) "
+   read -u 1 install_it
+   echo
+   if [ "$install_it" == "y" ]; then
+      echo                                                                                                          >> ~/.bashrc
+      echo "source $CSV2RDF4LOD_HOME/my-csv2rdf4lod-source-me.${ext} # http://purl.org/twc/id/software/csv2rdf4lod" >> ~/.bashrc
+   fi
+
+   echo
+   echo "~~~ What to do next: ~~~"
+   echo
+   echo "    running 'cr-vars.${ext} --check' to checks for dependencies that csv2rdf4lod-automation needs to run."
+   echo "    running 'install-csv2rdf4lod-dependencies.sh' installs dependencies."
+
+
+   echo
+   echo -n "Try to install dependencies? (y/N) "
+   read -u 1 install_it
+   echo
+   if [ "$install_it" == "y" ]; then
+      install-csv2rdf4lod-dependencies.sh
+   fi
+
+   echo
+   echo "~~~ What to do next: ~~~"
+   echo
+   echo "    running `cr-vars.${ext}` shows the environment variables that csv2rdf4lod-automation uses to control execution flow."
+   echo "      (see https://github.com/timrdf/csv2rdf4lod-automation/wiki/Installing-csv2rdf4lod-automation)"
+
+   echo
+   echo -n "run cr-vars.sh now? (Y/n) "
+   read -u 1 install_it
+   echo
+   if [ "$install_it" != "n" ]; then
+      cr-vars.sh
+   fi
+
+   echo
+   echo "Installation is complete. Check out the following for next steps:"
+   echo "* https://github.com/timrdf/csv2rdf4lod-automation/wiki/Installing-csv2rdf4lod-automation"
+   echo "* https://github.com/timrdf/csv2rdf4lod-automation/wiki/A-quick-and-easy-conversion"
+   echo "* https://github.com/timrdf/csv2rdf4lod-automation/wiki/Conversion-process-phases"
+   echo
 fi
-
-echo
-echo "~~~ What to do next: ~~~"
-echo
-echo "    running 'cr-vars.${ext} --check' to checks for dependencies that csv2rdf4lod-automation needs to run."
-echo "    running 'install-csv2rdf4lod-dependencies.sh' installs dependencies."
-
-
-echo
-echo -n "Try to install dependencies? (y/N) "
-read -u 1 install_it
-echo
-if [ "$install_it" == "y" ]; then
-   install-csv2rdf4lod-dependencies.sh
-fi
-
-echo
-echo "~~~ What to do next: ~~~"
-echo
-echo "    running `cr-vars.${ext}` shows the environment variables that csv2rdf4lod-automation uses to control execution flow."
-echo "      (see https://github.com/timrdf/csv2rdf4lod-automation/wiki/Installing-csv2rdf4lod-automation)"
-
-echo
-echo -n "run cr-vars.sh now? (Y/n) "
-read -u 1 install_it
-echo
-if [ "$install_it" != "n" ]; then
-   cr-vars.sh
-fi
-
-echo
-echo "Installation is complete. Check out the following for next steps:"
-echo "* https://github.com/timrdf/csv2rdf4lod-automation/wiki/Installing-csv2rdf4lod-automation"
-echo "* https://github.com/timrdf/csv2rdf4lod-automation/wiki/A-quick-and-easy-conversion"
-echo "* https://github.com/timrdf/csv2rdf4lod-automation/wiki/Conversion-process-phases"
-echo
