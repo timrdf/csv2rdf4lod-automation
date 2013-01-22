5fb914a251f2dbe3808176f2e48200ddc24e92eb
Timothy Lebo <timrdf@gmail.com>
2013-01-22 12:11:27 -0500

    value of line num

--- bin/util/cr-value-of.sh
+++ bin/util/cr-value-of.sh
@@ -11,20 +11,41 @@
 #
 #     bin/util/cr-value-of.sh CSV2RDF4LOD_CONVERT_OMIT_RAW_LAYER bin/setup.sh
 #       false
+#
+#
+#   When:
+#     grep --line-number CSV2RDF4LOD_CONVERT_OMIT_RAW_LAYER bin/setup.sh
+#       187:export CSV2RDF4LOD_CONVERT_OMIT_RAW_LAYER="true"
+#       188:export CSV2RDF4LOD_CONVERT_OMIT_RAW_LAYER="false"
+#
+#     bin/util/cr-value-of.sh CSV2RDF4LOD_CONVERT_OMIT_RAW_LAYER bin/setup.sh
+#       false
+#
+#     bin/util/cr-value-of.sh CSV2RDF4LOD_CONVERT_OMIT_RAW_LAYER bin/setup.sh --line-number
+#       188
 
-if [[ $# -ne 2 || "$1" == "--help" ]]; then
-   echo "usage: `basename $0` <CSV2RDF4LOD_> <source-me.sh>"
+
+if [[ $# -lt 2 || $# -gt 3 || "$1" == "--help" ]]; then
+   echo "usage: `basename $0` <CSV2RDF4LOD_> <source-me.sh> [--line-number]"
    echo
-   echo "  CSV2RDF4LOD_ : The environment variable to show the value of (e.g. CSV2RDF4LOD_CONVERT_OMIT_RAW_LAYER)."
-   echo "  source-me.sh : The file from which to determine the value    (e.g. my-csv2rdf4lod-source-me.sh)."
+   echo "  CSV2RDF4LOD_  : The environment variable to show the value of (e.g. CSV2RDF4LOD_CONVERT_OMIT_RAW_LAYER)."
+   echo "  source-me.sh  : The file from which to determine the value    (e.g. my-csv2rdf4lod-source-me.sh)."
+   echo "  --line-number : Instead of returning the value, return the line number on which the variable is *last* set."
 fi
 
 variable="$1"
 source_me="$2"
 
 if [ -e $source_me ]; then
-   #                       Should handle both ="" and ='' ------+
-   #                                                           \|/
-   #                                                            .
-   grep "export ${variable}=" $source_me | tail -1 | sed "s/^.*=.\(.*\)[\"']/\1/"
+   if [ "$3" == "--line-number" ]; then
+      #                              Should grab only integers ------+
+      #                                                             \|/
+      #                                                              .
+      grep --line-number "export ${variable}=" $source_me | sed 's/[\d]*:.*$//' | tail -1
+   else
+      #                       Should handle both ="" and ='' ------+
+      #                                                           \|/
+      #                                                            .
+      grep "export ${variable}=" $source_me | tail -1 | sed "s/^.*=.\(.*\)[\"']/\1/"
+   fi
 fi
