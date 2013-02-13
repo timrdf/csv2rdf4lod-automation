0c967158a4416cf8764be4dfb179f3f77776953c
Timothy Lebo <timrdf@gmail.com>
2013-02-12 20:52:10 -0500

    flag to make turtle

--- bin/util/uri-nodes.sh
+++ bin/util/uri-nodes.sh
@@ -10,15 +10,22 @@
 
 if [[ $# -eq 0 || "$1" == "--help" ]]; then
    echo
-   echo "usage: `basename $0` <some.rdf>*"
+   echo "usage: `basename $0` [--as-ttl [type-uri]] <some.rdf>*"
    echo "  Print the URI subjects and objects of the given RDF file."
+   echo "  --as-ttl [class-uri] - output the list as a valid N-TRIPLES file, where the nodes are typed to rdfs:Resource."
+   echo "           [class-uri] - use this class instead of rdfs:Resource (must be full URI, no <>)"
    exit
 fi
 
-version=''
-if [[ "$1" == "--version" && $# -gt 1 ]]; then
-   version="$2"
-   shift 2
+as_ttl=''
+class="rdfs:Resource"
+if [[ "$1" == "--as-ttl" ]]; then
+   as_ttl="$2"
+   if [[ "$#" -gt 1 && ! -e "$2" ]]; then
+      class=$2
+      shift
+   fi
+   shift
 fi
 
 total=$#
@@ -29,14 +36,20 @@ while [ $# -gt 0 ]; do
       continue
    fi
 
-   if [[ $total -eq 1 && `gzipped.sh $file` == "yes" && `guess-syntax.sh $file mime` == "text/plain" ]]; then
-      # Avoids dumping to an intermediate file.
-      # e.g. 2.0 GB unzipped ntriples file can be done in 1.5 minutes (as opposed to 4.5 minutes).
-      gunzip -c             $file | awk '$1 ~ /^<.*/ { print $1 } $3 ~ /^<.*/ { print $3 }'
+   if [[ -n "$as_ttl" ]]; then
+      echo "@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> ."
+      echo
+      $0 $* | awk -v class=$class '$1 ~ /^</ {print $1,"a",class,"."}'
    else
-      echo ".${total}. .`gzipped.sh $file`. .`guess-syntax.sh $file mime`." >&2
-      # Handles any syntax, compressed or not.
-      rdf2nt.sh --version 2 $file | awk '$1 ~ /^<.*/ { print $1 } $3 ~ /^<.*/ { print $3 }'
+      if [[ $total -eq 1 && `gzipped.sh $file` == "yes" && `guess-syntax.sh $file mime` == "text/plain" ]]; then
+         # Avoids dumping to an intermediate file.
+         # e.g. 2.0 GB unzipped ntriples file can be done in 1.5 minutes (as opposed to 4.5 minutes).
+         gunzip -c             $file | awk '$1 ~ /^<.*/ { print $1 } $3 ~ /^<.*/ { print $3 }'
+      else
+         #echo ".${total}. .`gzipped.sh $file`. .`guess-syntax.sh $file mime`." >&2
+         # Handles any syntax, compressed or not.
+         rdf2nt.sh --version 2 $file | awk '$1 ~ /^<.*/ { print $1 } $3 ~ /^<.*/ { print $3 }'
+      fi
    fi
 
    shift
