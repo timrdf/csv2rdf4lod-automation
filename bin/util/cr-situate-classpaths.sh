43cb5d6fda07b9a4bd26a007a4b37173d22bf665
Tim L <timrdf@gmail.com>
2013-02-28 21:10:37 -0500

    updated slf4j

--- bin/util/cr-situate-classpaths.sh
+++ bin/util/cr-situate-classpaths.sh
@@ -38,8 +38,8 @@ missing=""
 for jar in                                                             \
            lib/javacsv2.0/javacsv.jar                                  \
            bin/dup/openrdf-sesame-2.3.1-onejar.jar                     \
-           bin/dup/slf4j-api-1.5.6.jar                                 \
-           bin/dup/slf4j-nop-1.5.6.jar                                 \
+           lib/slf4j/slf4j-api-1.7.2.jar                               \
+           lib/slf4j/slf4j-nop-1.7.2.jar                               \
            lib/joda-time-2.0/joda-time-2.0.jar                         \
            bin/dup/datadigest-1.0-SNAPSHOT.jar                         \
            bin/lib/commons-validator-1.3.1/commons-validator-1.3.1.jar \
