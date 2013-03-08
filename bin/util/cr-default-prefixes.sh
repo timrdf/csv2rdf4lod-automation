83af171b42b776ac0a48975cc13a6f5b4dc5aad2
Tim L <timrdf@gmail.com>
2013-03-08 12:13:41 -0500

    vsr default prefix

--- bin/util/cr-default-prefixes.sh
+++ bin/util/cr-default-prefixes.sh
@@ -50,6 +50,7 @@ else
    echo "prefix dbpedia:    <http://dbpedia.org/resource/>"
    echo "prefix prov:       <http://www.w3.org/ns/prov#>"
    echo "prefix sio:        <http://semanticscience.org/resource/>"
+   echo "prefix vsr:        <http://purl.org/twc/vocab/vsr#>"
    echo "prefix twi:        <http://tw.rpi.edu/instances/>"
    echo
    # curl http://prefix.cc/rdfs,void,conversion.file.sparql | grep PREFIX
