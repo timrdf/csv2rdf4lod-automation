8d7dee0bb24c8c0d4de9846163cb229143358ac4
Tim L <timrdf@gmail.com>
2013-04-17 16:47:49 -0400

    removing <> in URI listings

--- bin/util/uri-nodes.sh
+++ bin/util/uri-nodes.sh
@@ -44,11 +44,11 @@ while [ $# -gt 0 ]; do
       if [[ $total -eq 1 && `gzipped.sh $file` == "yes" && `guess-syntax.sh $file mime` == "text/plain" ]]; then
          # Avoids dumping to an intermediate file.
          # e.g. 2.0 GB unzipped ntriples file can be done in 1.5 minutes (as opposed to 4.5 minutes).
-         gunzip -c             $file | awk '$1 ~ /^<.*/ { print $1 } $3 ~ /^<.*/ { print $3 }'
+         gunzip -c             $file | awk '$1 ~ /^<.*/ { print $1 } $3 ~ /^<.*/ { print $3 }' | sed 's/^<//;s/>$//'
       else
          #echo ".${total}. .`gzipped.sh $file`. .`guess-syntax.sh $file mime`." >&2
          # Handles any syntax, compressed or not.
-         rdf2nt.sh --version 2 $file | awk '$1 ~ /^<.*/ { print $1 } $3 ~ /^<.*/ { print $3 }'
+         rdf2nt.sh --version 2 $file | awk '$1 ~ /^<.*/ { print $1 } $3 ~ /^<.*/ { print $3 }' | sed 's/^<//;s/>$//'
       fi
    fi
 
