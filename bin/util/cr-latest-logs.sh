881d50d5776ee142c9ebca87f52de9af9bedd874
Tim L <timrdf@gmail.com>
2013-05-04 15:20:04 -0400

    cron log

--- bin/util/cr-latest-logs.sh
+++ bin/util/cr-latest-logs.sh
@@ -6,7 +6,7 @@ see='https://github.com/timrdf/csv2rdf4lod-automation/wiki/CSV2RDF4LOD-not-set'
 CSV2RDF4LOD_HOME=${CSV2RDF4LOD_HOME:?"not set; source csv2rdf4lod/source-me.sh or see $see"}
 
 # cr:data-root cr:source cr:directory-of-datasets cr:dataset cr:directory-of-versions cr:conversion-cockpit
-ACCEPTABLE_PWDs="cr:conversion-cockpit"
+ACCEPTABLE_PWDs="cr:conversion-cockpit cr:directory-of-versions"
 if [ `${CSV2RDF4LOD_HOME}/bin/util/is-pwd-a.sh $ACCEPTABLE_PWDs` != "yes" ]; then
    ${CSV2RDF4LOD_HOME}/bin/util/pwd-not-a.sh $ACCEPTABLE_PWDs
    exit 1
