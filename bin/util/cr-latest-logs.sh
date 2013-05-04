a4bc8adc12caf6fe1856196ddb676b39c0108cf9
Tim L <timrdf@gmail.com>
2013-05-04 15:19:36 -0400

    cron log

--- bin/util/cr-latest-logs.sh
+++ bin/util/cr-latest-logs.sh
@@ -12,8 +12,37 @@ if [ `${CSV2RDF4LOD_HOME}/bin/util/is-pwd-a.sh $ACCEPTABLE_PWDs` != "yes" ]; the
    exit 1
 fi
 
-if [ -e doc/logs/conversion-trigger-last-pulled ]; then
-   find doc/logs -type f -newer doc/logs/conversion-trigger-last-pulled
-else
-   ls -lt doc/logs 2> /dev/null | grep -v "total" | head -1 | awk '{print "doc/logs/"$NF}'
+if [[ `is-pwd-a.sh cr:conversion-cockpit` == "yes" ]]; then
+
+   if [ -e doc/logs/conversion-trigger-last-pulled ]; then
+      find doc/logs -type f -newer doc/logs/conversion-trigger-last-pulled
+   else
+      ls -lt doc/logs 2> /dev/null | grep -v "total" | head -1 | awk '{print "doc/logs/"$NF}'
+   fi
+
+elif [[ `is-pwd-a.sh cr:directory-of-versions` == "yes" ]]; then
+   for next in `cr-list-versions.sh | tail -1`; do
+      pushd $next > /dev/null
+         # Recursive call to base case 'cr:conversion-cockpit'
+         $0 $*
+      popd > /dev/null
+   done
+elif [[ `is-pwd-a.sh cr:data-root cr:source cr:directory-of-datasets` == "yes" ]]; then
+   for next in `directories.sh`; do
+      pushd $next > /dev/null
+         # Recursive call to base case 'cr:conversion-cockpit'
+         $0 $*
+      popd > /dev/null
+   done
+elif [[ `is-pwd-a.sh              cr:source                                                             ` == "yes" ]]; then
+   # TODO
+   pushd dataset > /dev/null
+      # Recursive call to base case 'cr:conversion-cockpit'
+      $0 $*
+   popd > /dev/null
+elif [[ `is-pwd-a.sh                                                 cr:dataset                         ` == "yes" ]]; then
+   pushd version > /dev/null
+      # Recursive call to base case 'cr:conversion-cockpit'
+      $0 $*
+   popd > /dev/null
 fi
