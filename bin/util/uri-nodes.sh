e1122501bd4447c7570e50a867c9bd3da5cea6b0
Tim L <timrdf@gmail.com>
2013-05-21 11:34:24 -0400

    --verbose

--- bin/util/uri-nodes.sh
+++ bin/util/uri-nodes.sh
@@ -10,11 +10,18 @@
 
 if [[ $# -eq 0 || "$1" == "--help" ]]; then
    echo
-   echo "usage: `basename $0` [--as-ttl [type-uri]] <some.rdf>*"
+   echo "usage: `basename $0` [-v] [--as-ttl [type-uri]] <some.rdf>*"
    echo "  Print the URI subjects and objects of the given RDF file."
+   echo "        -v             - verbose"
    echo "  --as-ttl [class-uri] - output the list as a valid N-TRIPLES file, where the nodes are typed to rdfs:Resource."
    echo "           [class-uri] - use this class instead of rdfs:Resource (must be full URI, no <>)"
    exit
+fi
+
+verbose=""
+if [[ "$1" == "-v" || "$1" == "--verbose" ]]; then
+   verbose="yes"
+   shift
 fi
 
 as_ttl=''
@@ -31,6 +38,7 @@ fi
 total=$#
 while [ $# -gt 0 ]; do
    file="$1" 
+   shift
 
    if [ ! -f $file ]; then
       continue
@@ -39,18 +47,26 @@ while [ $# -gt 0 ]; do
    if [[ -n "$as_ttl" ]]; then
       echo "@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> ."
       echo
+      if [[ -n "$verbose" ]]; then
+         echo $0 $* | awk -v class=$class '$1 ~ /^</ {print $1,"a",class,"."}'
+      fi
       $0 $* | awk -v class=$class '$1 ~ /^</ {print $1,"a",class,"."}'
    else
       if [[ $total -eq 1 && `gzipped.sh $file` == "yes" && `guess-syntax.sh $file mime` == "text/plain" ]]; then
          # Avoids dumping to an intermediate file.
          # e.g. 2.0 GB unzipped ntriples file can be done in 1.5 minutes (as opposed to 4.5 minutes).
+         if [[ -n "$verbose" ]]; then
+            echo gunzip -c $file | awk '$1 ~ /^<.*/ { print $1 } $3 ~ /^<.*/ { print $3 }' | sed 's/^<//;s/>$//'
+         fi
          gunzip -c $file | awk '$1 ~ /^<.*/ { print $1 } $3 ~ /^<.*/ { print $3 }' | sed 's/^<//;s/>$//'
       else
          #echo ".${total}. .`gzipped.sh $file`. .`guess-syntax.sh $file mime`." >&2
          # Handles any syntax, compressed or not.
+         if [[ -n "$verbose" ]]; then
+            echo rdf2nt.sh $file | awk '$1 ~ /^<.*/ { print $1 } $3 ~ /^<.*/ { print $3 }' | sed 's/^<//;s/>$//'
+         fi
          rdf2nt.sh $file | awk '$1 ~ /^<.*/ { print $1 } $3 ~ /^<.*/ { print $3 }' | sed 's/^<//;s/>$//'
       fi
    fi
 
-   shift
 done
