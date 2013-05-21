faeae3c0e8dffa5dcd74baa1b40660929041f742
Tim L <timrdf@gmail.com>
2013-05-21 19:18:50 -0400

    xsd prefix

--- bin/util/cr-full-dump.sh
+++ bin/util/cr-full-dump.sh
@@ -242,6 +242,7 @@ if [ "$dryrun" != "true" ]; then
    echo "$cockpit/publish/$sdv.ephemeral.ttl (void:triples)"
    triples=`rdf2nt.sh $cockpit/publish/$dumpFileLocal | rapper -i ntriples -c -I http://blah - 2>&1 | awk '$0~/Parsing returned/{print $4}'`
    if [[ ${#triples} -gt 0 && $triples == [0-9]* ]]; then # - - - - - - - - - - Avoid publish/*.void.ttl pattern so that cr-publish-void-to-endpoint.sh doesn't find it.
+      echo "@prefix xsd:     <http://www.w3.org/2001/XMLSchema#> ."                                                                  >> $cockpit/publish/$sdv.ephemeral.ttl
       echo "@prefix dcterms: <http://purl.org/dc/terms/> ."                                                                          >> $cockpit/publish/$sdv.ephemeral.ttl
       echo "@prefix void:    <http://rdfs.org/ns/void#> ."                                                                           >> $cockpit/publish/$sdv.ephemeral.ttl
       echo "@prefix dat:     <http://www.w3.org/ns/dcat#> ."                                                                         >> $cockpit/publish/$sdv.ephemeral.ttl
