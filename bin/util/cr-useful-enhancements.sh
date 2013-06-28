f10d551a8d310ae05d08d247994b2d34073574ae
Tim L <timrdf@gmail.com>
2013-06-28 09:26:09 -0400

    useful eparams

---
+++ bin/util/cr-useful-enhancements.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+#3> <> prov:specializationOf <https://github.com/timrdf/csv2rdf4lod-automation/blob/master/bin/util/install-csv2rdf4lod-dependencies.sh> .
+
+HOME=$(cd ${0%/*/*} && echo ${PWD%/*})
+export PATH=$PATH`$HOME/bin/util/cr-situate-paths.sh`
+export CLASSPATH=$CLASSPATH`$HOME/bin/util/cr-situate-classpaths.sh`
+CSV2RDF4LOD_HOME=${CSV2RDF4LOD_HOME:?$HOME}
+
+while [ $# -gt 0 ]; do
+   eparams="$1"
+   if [ -e $eparams ]; then
+      java edu.rpi.tw.data.csv.impl.UsefulEnhancements $eparams
+   fi
+done
