c0c4d9646a71bef9cec7e3519a4ea222deea003e
Tim L <timrdf@gmail.com>
2013-08-28 00:42:48 -0400

    depicts debug

--- bin/util/o-of-p.sh
+++ bin/util/o-of-p.sh
@@ -51,7 +51,7 @@ while [ $# -gt 0 ]; do
       echo
       $0 $predicate $* | awk -v class=$class '$1 ~ /^</ {print $1,"a",class,"."}'
    else
-      echo hi
+      echo hi >&2
       if [[ $total -eq 1 && `gzipped.sh $file` == "yes" && `guess-syntax.sh $file mime` == "text/plain" ]]; then
          # Avoids dumping to an intermediate file.
          # e.g. 2.0 GB unzipped ntriples file can be done in 1.5 minutes (as opposed to 4.5 minutes).
