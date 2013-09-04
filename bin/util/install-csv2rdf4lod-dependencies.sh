15dabc65bc5d5b2c43ef3fa4961923739e17a580
Tim L <timrdf@gmail.com>
2013-09-03 21:47:43 -0400

    rdflib version

--- bin/util/install-csv2rdf4lod-dependencies.sh
+++ bin/util/install-csv2rdf4lod-dependencies.sh
@@ -519,6 +519,7 @@ for egg in $eggs; do
    find /usr/local/lib/python$V/dist-packages -mindepth 1 -maxdepth 1 | grep -i $eggReg &> /dev/null
    status=$?
    #there=`find /usr/local/lib/python$V/dist-packages -mindepth 1 -maxdepth 1 -type d | grep -i $eggReg`
+   echo find /usr/local/lib/python$V/dist-packages -mindepth 1 -maxdepth 1 -type d -name "$eggReg*"
    there=`find /usr/local/lib/python$V/dist-packages -mindepth 1 -maxdepth 1 -type d -name "$eggReg*"`
    status=$?
    if [[ -e "$there" && "$there" =~ /usr/*.egg ]]; then # TODO: this path is $base/python/lib/site-packages if -z $sudo
