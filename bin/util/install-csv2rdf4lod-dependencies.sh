7a3f2065e70cea1b03dbbd0fd47330fdbaa0ace2
Tim L <timrdf@gmail.com>
2013-09-03 22:18:39 -0400

    rdflib version

--- bin/util/install-csv2rdf4lod-dependencies.sh
+++ bin/util/install-csv2rdf4lod-dependencies.sh
@@ -532,13 +532,13 @@ for egg in $eggs; do
       else
          eggV=''
       fi
-      echo $TODO $sudo easy_install -U $egg$eggV
+      echo $TODO $sudo easy_install --install-dir /usr/local/lib/python$V/dist-packages -U $egg$eggV
       if [[ "$dryrun" != "true" ]]; then
          echo
          read -p "Q: Try to install python module $egg using the command above? (y/n) " -u 1 install_it
          if [[ "$install_it" == [yY] ]]; then
             echo
-                 $sudo easy_install -U "$egg$eggV"
+                 $sudo easy_install --install-dir /usr/local/lib/python$V/dist-packages -U "$egg$eggV"
                 # SUDO IS NOT REQUIRED HERE.
             # see https://github.com/timrdf/csv2rdf4lod-automation/wiki/Installing-csv2rdf4lod-automation---complete
             pdiv=""
