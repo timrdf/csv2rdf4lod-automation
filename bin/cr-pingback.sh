cb8c3fcaae322bbbfc690a7ed3560442c121ccbf
Tim L <timrdf@gmail.com>
2013-09-04 15:49:27 -0400

    pingback

--- bin/cr-pingback.sh
+++ bin/cr-pingback.sh
@@ -119,9 +119,13 @@ if [[ -z "$within_last_week" || "$force" == "true" ]]; then
    curl -sH "Accept: application/rdf+xml" -L ${CSV2RDF4LOD_BASE_URI_OVERRIDE:-$CSV2RDF4LOD_BASE_URI}/void > $cockpit/source/void.rdf
    if [[ -e $opt/DataFAQs/services/sadi/ckan/add-metadata.py ]]; then
       echo "http://datahub.io/dataset/$CSV2RDF4LOD_PUBLISH_DATAHUB_METADATA_OUR_BUBBLE_ID <- $cockpit/source/void.rdf"
+      echo "add-metadata.py's input:"
       valid-rdf.sh -v $cockpit/source/void.rdf
       void-triples.sh $cockpit/source/void.rdf
       python $opt/DataFAQs/services/sadi/ckan/add-metadata.py $cockpit/source/void.rdf > $cockpit/source/response.rdf
+      echo "add-metadata.py's output:"
+      valid-rdf.sh -v $cockpit/source/response.rdf
+      void-triples.sh $cockpit/source/response.rdf
       cat $cockpit/source/response.rdf
    else
       echo "ERROR: `basename $0` could not find $opt/DataFAQs/services/sadi/ckan/add-metadata.py"
