dec358a556b4aef16b44407098c36474465631e7
Tim L <timrdf@gmail.com>
2013-09-13 09:17:12 -0400

    moving isdefinedby into enabled dataset

--- bin/dataset/cr-isdefinedby.sh
+++ bin/dataset/cr-isdefinedby.sh
@@ -46,7 +46,7 @@ elif [[ `is-pwd-a.sh 'cr:data-root'` == "yes" ]]; then
 else
    sourceID=`cr-source-id.sh`
 fi
-datasetID=`basename $me | sed -e 's/.sh$//'`
+datasetID=`basename $this | sed -e 's/.sh$//'`
 if [[ "$1" != "" ]]; then
    versionID="$1"
 elif [[ `is-pwd-a.sh 'cr:conversion-cockpit'` == "yes" ]]; then
