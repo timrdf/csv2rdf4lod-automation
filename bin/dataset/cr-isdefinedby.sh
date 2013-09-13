8d3b80fefebf788697fc0303f7a156390a406d97
Tim L <timrdf@gmail.com>
2013-09-13 08:39:21 -0400

    moving isdefinedby into enabled dataset

--- bin/dataset/cr-isdefinedby.sh
+++ bin/dataset/cr-isdefinedby.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 #
-# https://github.com/timrdf/csv2rdf4lod-automation/blob/master/bin/cr-publish-isdefinedby-to-endpoint.sh
-#
-# See also:
-#    https://github.com/timrdf/csv2rdf4lod-automation/wiki/Aggregating-subsets-of-converted-datasets
+#3> <> a conversion:RetrievalTrigger;
+#3>    prov:specializationOf <https://github.com/timrdf/csv2rdf4lod-automation/blob/master/bin/cr-publish-isdefinedby-to-endpoint.sh>;
+#3>    rdfs:seeAlso          <https://github.com/timrdf/csv2rdf4lod-automation/wiki/Aggregating-subsets-of-converted-datasets> .
 #
 # Environment variables used:
 #
@@ -23,13 +22,18 @@
 # Example usage:
 # 
 
-see="https://github.com/timrdf/csv2rdf4lod-automation/wiki/CSV2RDF4LOD-not-set"
-CSV2RDF4LOD_HOME=${CSV2RDF4LOD_HOME:?"not set; source csv2rdf4lod/source-me.sh or see $see"}
+[ -n "`readlink $0`" ] && this=`readlink $0` || this=$0
+HOME=$(cd ${this%/*/*/*} && pwd)
+export PATH=$PATH`$HOME/bin/install/paths.sh`
+export CLASSPATH=$CLASSPATH`$HOME/bin/install/classpaths.sh`
+
+echo $this
+echo $HOME
 
 # cr:data-root cr:source cr:directory-of-datasets cr:dataset cr:directory-of-versions cr:conversion-cockpit
 ACCEPTABLE_PWDs="cr:data-root cr:source cr:dataset cr:directory-of-versions"
-if [ `${CSV2RDF4LOD_HOME}/bin/util/is-pwd-a.sh $ACCEPTABLE_PWDs` != "yes" ]; then
-   ${CSV2RDF4LOD_HOME}/bin/util/pwd-not-a.sh $ACCEPTABLE_PWDs
+if [ `is-pwd-a.sh $ACCEPTABLE_PWDs` != "yes" ]; then
+   pwd-not-a.sh $ACCEPTABLE_PWDs
    exit 1
 fi
 
@@ -39,7 +43,7 @@ sourceID=$CSV2RDF4LOD_PUBLISH_OUR_SOURCE_ID
 datasetID=`basename $0 | sed -e 's/-publish//' -e 's/-to-endpoint//' -e 's/.sh$//'` # e.g. cr-publish-void-to-endpoint.sh -> cr-void
 versionID=`date +%Y-%b-%d`
 
-graphName=${CSV2RDF4LOD_BASE_URI_OVERRIDE:-$CSV2RDF4LOD_BASE_URI}/source/$sourceID/dataset/$datasetID/version/$versionID
+exit
 
 if [[ $# -lt 1 || "$1" == "--help" ]]; then
    echo "usage: `basename $0` [--target] [-n] --clear-graph <named_graph_URI | cr:auto | .>"
@@ -58,28 +62,10 @@ if [[ $# -lt 1 || "$1" == "--help" ]]; then
    exit 1
 fi
 
-if [ "$1" == "--target" ]; then
-   # a conversion:VersionedDataset:
-   # e.g. http://purl.org/twc/health/source/tw-rpi-edu/dataset/cr-publish-tic-to-endpoint/version/2012-Sep-07
-   echo $graphName
-   exit 0
-fi
-
 dryRun="false"
 if [ "$1" == "-n" ]; then
    dryRun="true"
    dryrun.sh $dryrun beginning
-   shift 
-fi
-
-clearGraph="false"
-if [ "$1" == "--clear-graph" ]; then
-   clearGraph="true"
-   shift
-fi
-
-if [ "$1" != "cr:auto" ]; then
-   graphName="$1"
    shift 
 fi
 
@@ -113,23 +99,6 @@ if [[ `is-pwd-a.sh                                                              
          # ^^ publishes if CSV2RDF4LOD_PUBLISH_VIRTUOSO
       popd &> /dev/null
    fi
-
-   if [ "$clearGraph" == "true" ]; then
-      echo ""
-      echo "Deleting $graphName" >&2
-      if [ "$dryRun" != "true" ]; then
-         publish/bin/virtuoso-delete-$sourceID-$datasetID-$versionID.sh
-      fi
-   fi
-
-   if [ "$dryRun" != "true" ]; then
-      pushd $cockpit &> /dev/null
-         publish/bin/virtuoso-load-$sourceID-$datasetID-$versionID.sh
-      popd &> /dev/null
-   fi
-
-   # if [ "$CSV2RDF4LOD_PUBLISH_COMPRESS" == "true" ]; then
-   # fi
 
    dryrun.sh $dryrun ending
 elif [[ `is-pwd-a.sh                                                 cr:dataset`                          == "yes" ]]; then
