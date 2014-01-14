b2ecc696ddf652ff7a46a01b64a3806a967fee59
Tim L <timrdf@gmail.com>
2014-01-14 15:00:05 -0500

    capturing cr-linksets to log in cron

--- bin/cr-cron.sh
+++ bin/cr-cron.sh
@@ -198,7 +198,7 @@ pushd $conversion_root &> /dev/null
          -n "$CSV2RDF4LOD_PUBLISH_DATAHUB_METADATA_OUR_BUBBLE_ID" && \
          `which cr-linksets.sh` ]]; then
       echo "pwd: `pwd`"                                                                                                    >> $log
-      cr-linksets.sh
+      cr-linksets.sh                                                                                                       >> $log
    else
       echo "   ERROR: Failed to invoke:"                                                                                   >> $log
       echo "      CSV2RDF4LOD_BASE_URI:              $CSV2RDF4LOD_BASE_URI"                                                >> $log
