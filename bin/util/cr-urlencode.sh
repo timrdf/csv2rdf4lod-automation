0cfc978f7f5abc38b1859eca92a112e28b811710
Tim L <timrdf@gmail.com>
2014-01-19 11:27:23 -0500

    --limit-offset argument for cache-queries.sh

--- bin/util/cr-urlencode.sh
+++ bin/util/cr-urlencode.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 #
 #3> <> prov:specializationOf <https://github.com/timrdf/csv2rdf4lod-automation/blob/master/bin/util/cr-urlencode.sh>;
-#3>    prov:wasDerivedFrom <http://stackoverflow.com/questions/296536/urlencode-from-a-bash-script>;
+#3>    prov:wasDerivedFrom   <http://stackoverflow.com/questions/296536/urlencode-from-a-bash-script>;
 #3> .
+#
+
+if [[ $# -eq 0 || "$1" == "--help" ]]; then
+   echo "usage: `basename $0` [--help] <string>+"
+   echo
+   echo "  e.g."
+   echo "      cr-urlencode.sh 'prefix xsd:    <http://www.w3.org/2001/XMLSchema#>'"
+   echo "      -> prefix%20xsd%3a%20%20%20%20%3chttp%3a%2f%2fwww.w3.org%2f2001%2fXMLSchema%23%3e"
+   exit
+fi
 
 rawurlencode() { # http://stackoverflow.com/questions/296536/urlencode-from-a-bash-script
   local string="${1}"
@@ -34,3 +44,5 @@ fi
 #encoded=`echo $target | perl -e 'use URI::Escape; @userinput = <STDIN>; foreach (@userinput) { chomp($_); print uri_escape($_); }'`
 #request=$service$encoded'&responseType=rdf'
 #echo $request
+
+# Or use curl --data-urlencode
