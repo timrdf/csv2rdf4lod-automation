b31c588f8d60096737a9591da93b918cf92c3997
Tim L <timrdf@gmail.com>
2014-01-23 10:30:54 -0500

    added date epoch hash naming

--- bin/util/resource-name.sh
+++ bin/util/resource-name.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+if [[ "$1" == "--help" ]]; then
+   echo "usage: `basename $0`"
+   echo
+   java -cp $CLASSPATH`$CSV2RDF4LOD_HOME/bin/util/cr-situate-classpaths.sh` edu.rpi.tw.string.NameFactory --help
+   exit
+fi
+
 java -cp $CLASSPATH`$CSV2RDF4LOD_HOME/bin/util/cr-situate-classpaths.sh` edu.rpi.tw.string.NameFactory $*
