ec1fbeaa7b8de3bca830fb5a743fc4d0aa5c0cd1
Tim L <timrdf@gmail.com>
2014-03-09 00:13:21 -0500

    added --instances-of to o-of-p.sh to list instances of a given type

--- bin/util/o-of-p.sh
+++ bin/util/o-of-p.sh
@@ -10,10 +10,13 @@
 
 if [[ $# -eq 0 || "$1" == "--help" ]]; then
    echo
-   echo "usage: `basename $0` [--as-ttl [type-uri]] --inverse-of <predicate> <some.rdf>*"
+   echo "usage: `basename $0` [--as-ttl [type-uri]] [--inverse-of] [--instances-of] <predicate> <some.rdf>*"
    echo "  Print the URI subjects and objects of the given RDF file."
+   echo
    echo "  --as-ttl [class-uri] - output the list as a valid Trutle file, where the nodes are typed to rdfs:Resource."
-   echo "           [class-uri] - use this class instead of rdfs:Resource (must be full URI, no <>)"
+   echo "           [class-uri] - use this class instead of rdfs:Resource (must be full URI, no <>)."
+   echo "          --inverse-of - print the subjects of the predicate (instead of the objects)."
+   echo "          --instances-of - print the subjects of the predicate (instead of the objects) (note: <predicate> is interpreted as a class)."
    exit
 fi
 
@@ -31,6 +34,12 @@ fi
 inverse=""
 if [[ "$1" == "--inverse-of" ]]; then
    inverse="yes"
+   shift
+fi
+
+instances=""
+if [[ "$1" == "--instances-of" ]]; then
+   instances="yes"
    shift
 fi
 
@@ -55,16 +64,22 @@ while [ $# -gt 0 ]; do
          #echo "short circuiting rdf2nt .${total}. .`gzipped.sh $file`. .`guess-syntax.sh $file mime`." >&2
          # Avoids dumping to an intermediate file.
          # e.g. 2.0 GB unzipped ntriples file can be done in 1.5 minutes (as opposed to 4.5 minutes).
-         gunzip -c             $file | awk -v p="<$predicate>" '{if($2 == p){ gsub("<",""); gsub(">",""); print $3 }}'
-         if [[ -n "$inverse" ]]; then
+         if [[ -n "$instances" ]]; then
+            gunzip -c             $file | awk -v c="<$predicate>" '{if($3 == c && $2 == "<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>"){ gsub("<",""); gsub(">",""); print $1 }}'
+         elif [[ -n "$inverse" ]]; then
             gunzip -c             $file | awk -v p="<$predicate>" '{if($2 == p){ gsub("<",""); gsub(">",""); print $1 }}'
+         else # TODO: this wasn't mutually exclusive. That was a bug, right?
+            gunzip -c             $file | awk -v p="<$predicate>" '{if($2 == p){ gsub("<",""); gsub(">",""); print $3 }}'
          fi
       else
          #echo "using rdf2nt .${total}. .`gzipped.sh $file`. .`guess-syntax.sh $file mime`." >&2
          # Handles any syntax, compressed or not.
-         rdf2nt.sh $file | awk -v p="<$predicate>" '{if($2 == p){ gsub("<",""); gsub(">",""); print $3 }}'
-         if [[ -n "$inverse" ]]; then
+         if [[ -n "$instances" ]]; then
+            rdf2nt.sh $file | awk -v c="<$predicate>" '{if($3 == c && $2 == "<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>"){ gsub("<",""); gsub(">",""); print $1 }}'
+         elif [[ -n "$inverse" ]]; then
             rdf2nt.sh $file | awk -v p="<$predicate>" '{if($2 == p){ gsub("<",""); gsub(">",""); print $1 }}'
+         else # TODO: this wasn't mutually exclusive. That was a bug, right?
+            rdf2nt.sh $file | awk -v p="<$predicate>" '{if($2 == p){ gsub("<",""); gsub(">",""); print $3 }}'
          fi
       fi
    fi
