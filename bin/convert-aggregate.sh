e82a0c9e1d443f97b4e20badf8fe4340d11113a6
Tim L <timrdf@gmail.com>
2015-04-19 12:35:57 -0400

    avoiding ln on pvdelete

--- bin/convert-aggregate.sh
+++ bin/convert-aggregate.sh
@@ -900,7 +900,7 @@ echo "   echo \"Refusing to publish; see 'cr:dev and refusing to publish' at\"" 
 echo "   echo \"  https://github.com/timrdf/csv2rdf4lod-automation/wiki/CSV2RDF4LOD-environment-variables-%28considerations-for-a-distributed-workflow%29\"" >> $vloadSH
 echo "   exit 1"                                                                                                            >> $vloadSH
 echo "fi"                                                                                                                   >> $vloadSH
-echo "if [[ -e '$lnwwwrootSH' && "\$action" == 'pvload.sh' ]]; then"                                                        >> $vloadSH
+echo "if [[ -e '$lnwwwrootSH' && \"\$action\" =~ pvload* ]]; then"                                                          >> $vloadSH
 echo "   # Make sure that the file we will load from the web is published"                                                  >> $vloadSH
 echo "   $lnwwwrootSH"                                                                                                      >> $vloadSH
 echo "fi"                                                                                                                   >> $vloadSH
