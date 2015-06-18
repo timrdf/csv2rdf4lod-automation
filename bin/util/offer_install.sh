41199c977258e92840b37a251e949fdc8952ca14
Tim L <timrdf@gmail.com>
2015-06-18 15:29:33 -0400

    offer_install

--- bin/util/offer_install.sh
+++ bin/util/offer_install.sh
@@ -67,7 +67,7 @@ function offer_install_with_yum_or_apt_ifnowhich {
             if [[ "$command" == '.' ]]; then
                if [[ -e "`$sudo which dpkg 2> /dev/null`" ]]; then
                   echo "[okay] $package already available:"
-                  echo dpkg -s $package
+                  dpkg -s $package
                else
                   echo "[okay] no dpkg (sudo=$sudo): `$sudo which dpkg`"
                fi
