90ae98676305e112501cf517645d1d256ff52c5e
Tim L <timrdf@gmail.com>
2015-06-18 14:45:38 -0400

    offer install exposed for prizms

--- bin/util/offer_install.sh
+++ bin/util/offer_install.sh
@@ -8,52 +8,54 @@ function offer_install_with_yum_or_apt_ifnowhich {
    # See also https://github.com/timrdf/DataFAQs/blob/master/bin/install-datafaqs-dependencies.sh
    # See also Prizms bin/install.sh
    if [[ -n "$sudo" ]]; then
+      # TODO: after this was moved to a separate file for use by others, it demands that the caller set it up for us...
       command="$1"
       package="$2"
-         if [[ -n "$command" && -n "$package" ]]; then
+      reason="$3" # TODO: implement this.
+      if [[ -n "$command" && -n "$package" ]]; then
 
-            already_there='no'
-            if [[ "$command" == '.' ]]; then
-               if [[ `which apt-get    &> /dev/null` && \
-                     `dpkg -s $package &> /dev/null` ]]; then # 0 is true, 1 is false
-                  already_there='yes'
-               elif [[ `which yum 2> /dev/null` ]]; then
-                  already_there='TODO'
-               fi
-            else
-               if [[ `$sudo which $command 2> /dev/null` ]]; then
-                  already_there='yes'
-               fi
+         already_there='no'
+         if [[ "$command" == '.' ]]; then
+            if [[ `which apt-get    &> /dev/null` && \
+                  `dpkg -s $package &> /dev/null` ]]; then # 0 is true, 1 is false
+               already_there='yes'
+            elif [[ `which yum 2> /dev/null` ]]; then
+               already_there='TODO'
             fi
-
-            #if [[ ! `$sudo which $command 2> /dev/null` ]]; then
-            if [[ "$already_there" != 'yes' ]]; then
-               if [ "$dryrun" != "true" ]; then
-                  echo
-               fi
-               if [[ `which apt-get 2> /dev/null` ]]; then
-                  echo $TODO $sudo apt-get install $package
-               elif [[ `which yum 2> /dev/null` ]]; then
-                  echo $TODO $sudo yum install $package
-               else
-                  echo "WARNING: how to install $package without apg-get or yum?"
-               fi
-               if [[ "$dryrun" != "true" && ( `which apt-get 2> /dev/null` || `which yum 2> /dev/null` ) ]]; then
-                  read -p "Q: Could not find $command on path. Try to install with command shown above? (y/n): " -u 1 install_it
-                  if [[ "$install_it" == [yY] ]]; then
-                     if [[ `which apt-get 2> /dev/null` ]]; then
-                        echo $sudo apt-get install $package
-                             $sudo apt-get install $package
-                     elif [[ `which yum 2> /dev/null` ]]; then
-                        echo $sudo yum install $package
-                             $sudo yum install $package
-                     fi
-                  fi
-               fi
-            else
-               echo "[okay] $command already available at `which $command 2> /dev/null`"
+         else
+            if [[ `$sudo which $command 2> /dev/null` ]]; then
+               already_there='yes'
             fi
          fi
+
+         #if [[ ! `$sudo which $command 2> /dev/null` ]]; then
+         if [[ "$already_there" != 'yes' ]]; then
+            if [ "$dryrun" != "true" ]; then
+               echo
+            fi
+            if [[ `which apt-get 2> /dev/null` ]]; then
+               echo $TODO $sudo apt-get install $package
+            elif [[ `which yum 2> /dev/null` ]]; then
+               echo $TODO $sudo yum install $package
+            else
+               echo "WARNING: how to install $package without apg-get or yum?"
+            fi
+            if [[ "$dryrun" != "true" && ( `which apt-get 2> /dev/null` || `which yum 2> /dev/null` ) ]]; then
+               read -p "Q: Could not find $command on path. Try to install with command shown above? (y/n): " -u 1 install_it
+               if [[ "$install_it" == [yY] ]]; then
+                  if [[ `which apt-get 2> /dev/null` ]]; then
+                     echo $sudo apt-get install $package
+                          $sudo apt-get install $package
+                  elif [[ `which yum 2> /dev/null` ]]; then
+                     echo $sudo yum install $package
+                          $sudo yum install $package
+                  fi
+               fi
+            fi
+         else
+            echo "[okay] $command already available at `which $command 2> /dev/null`"
+         fi
+      fi
       which $command >& /dev/null
       return $?
    else
