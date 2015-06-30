c84383be1ee157ea3e52309b6c215f0b68a8108a
Tim L <timrdf@gmail.com>
2015-06-30 15:40:49 -0400

    tweaks on triff

--- bin/util/triff.sh
+++ bin/util/triff.sh
@@ -2,21 +2,27 @@
 #3>    prov:wasDerivedFrom   <https://github.com/timrdf/prov-wg/issues/12>;
 #3> .
 
+if [[ "$1" == '--clean' ]]; then
+   rm ___triff___*
+   exit
+fi
+
 if [[ "$1" == '--help' || "$1" == '-h' || $# -ne 2 ]]; then
    echo "usage: $0"
    exit
 fi
 
- left="$1" &&  _left="_`basename $0`_left"  &&  _left_=${_left}_
-right="$2" && _right="_`basename $0`_right" && _right_=${_right_}_ 
+ left="$1"
+right="$2"
 
 function download() {
    file="$1"
-   link="_triff_`md5.sh -qs "$file"`"
+   link="___triff___`md5.sh -qs "$file"`"
    if [[ -e "$file" ]]; then
       ln -sf "$file" "$link"
    elif [[ "$file" =~ http* ]]; then
       curl -sL "$file" > $link
+      ln -sf "$file" $link.url
    fi
    echo $link
 }
@@ -26,6 +32,30 @@ function normalize() {
    norm="$file.norm.ttl"
    rdf2ttl.sh $file > $norm
    echo $norm
+}
+
+function sortnorm() {
+   file="$1"
+   sorted="$file.sorted.ttl"
+   cat "$file" | sort > $sorted
+   echo $sorted
+}
+
+function showdiff() {
+   local title="$1"
+   local  left="$2"
+   local  right="$3"
+   local _left="$4"
+   local _right="$5"
+   diffs=`diff --brief $_left $_right`
+   if [ ${#diffs} -gt 0 ]; then
+      echo "$lpad`pad ${#left} ' '`${#diffs} $title"
+      echo "$lpad // $left   <|>   $right"
+      echo $div
+      diff -y -W $yW "$_left" "$_right" | grep -v '^@prefix'
+   else
+      echo "$lpad`pad ${#left} ' '`${#diffs} $title" >&2
+   fi
 }
 
 function pad() {
@@ -40,22 +70,27 @@ lpadw=$(( ( $yw - ${#left} ) - 9 ))
 #rpadw=$(( ( ( $yw - ${#left} ) / 2 ) - 2 ))
 lpad=`head -c ${lpadw} < /dev/zero | tr '\0' ' '`
 #rpad=`head -c ${rpadw} < /dev/zero | tr '\0' ' '`
+div="`head -c $yW < /dev/zero | tr '\0' '-'`"
 
-_left=` download "$left"`
+_left=`download "$left"`
+_left_=`normalize "$_left"`
+_left__=`sortnorm "$_left_"`
 _right=`download "$right"`
+_right_=`normalize "$_right"`
+_right__=`sortnorm "$_right_"`
 
 diffs=`diff --brief $_left $_right`
 if [ ${#diffs} -gt 0 ]; then
    echo "$lpad`pad ${#left} ' '`${#diffs} textual differences"
 fi
 
-_left_=` normalize "$_left"`
-_right_=`normalize "$_right"`
-
 diffs=`diff --brief $_left_ $_right_`
 if [ ${#diffs} -gt 0 ]; then
    echo "$lpad`pad ${#left} ' '`${#diffs} normalized differences"
    echo "$lpad // $left   <|>   $right"
-   echo "`head -c $yW < /dev/zero | tr '\0' '-'`"
+   #echo "`head -c $yW < /dev/zero | tr '\0' '-'`"
+   echo $div
    diff -y -W $yW "$_left_" "$_right_" | grep -v '^@prefix'
 fi
+
+showdiff 'sorted normalized differences' "$left" "$right" "$_left__" "$_right__"
