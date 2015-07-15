1a660099663f6c7627c70b8c261e44e87b7c91f8
Tim L <timrdf@gmail.com>
2015-07-15 10:22:55 -0400

    create triggers to ease https://github.com/timrdf/csv2rdf4lod-automation/wiki/Triggers

---
+++ bin/cr-create-trigger.sh
@@ -0,0 +1,33 @@
+#!/bin/bash 
+me=$(cd ${0%/*} && echo ${PWD})/`basename $0`
+
+options=''
+for available in `find \`dirname $me\` -name "*\`basename $0\`.*"`; do
+   option="${available##*sh.}"
+   if [[ "$option" != 'swp' ]]; then
+      options="$options$sp${available##*sh.}"
+      sp=", "
+   fi
+done
+
+usage="usage: `basename $0` {$options}"
+
+if [[ `cr-pwd-type.sh` == 'cr:conversion-cockpit' ]]; then
+   if [[ -e "$me.$1" ]]; then
+      if [[ ! -e "$1.sh" ]]; then
+         cat "$me.$1" > "$1.sh"
+         chmod +x "$1.sh"
+         ls -lt "$1.sh"
+      else
+         echo "$1.sh already exists; skipping."
+      fi
+   else
+      if [[ $# -gt 0 ]]; then
+         echo "trigger type $1 not available; skipping."
+      fi
+      echo "$usage"
+   fi
+else
+   echo "not in a conversion cockpit; skipping."
+   echo "$usage"
+fi
